d9ec9a20546143e9070433a7e5c7cdd90838f95e
Boris <Glazunov2142@icloud.com>
2023-08-03 20:09:33 +0400

    feat: make controllers for server and passed into.

--- cmd/api/resource/resource_api_service.go
+++ cmd/api/resource/resource_api_service.go
@@ -2,6 +2,9 @@ package resource
 
 import (
 	"context"
+	"github.com/Borislavv/video-streaming/internal/infrastructure/api/v1/controller"
+	"github.com/Borislavv/video-streaming/internal/infrastructure/api/v1/controller/audio"
+	"github.com/Borislavv/video-streaming/internal/infrastructure/api/v1/controller/video"
 	"github.com/Borislavv/video-streaming/internal/infrastructure/server/http"
 	"sync"
 )
@@ -19,6 +22,19 @@ func (r *ResourcesApiService) Run() {
 	wg := &sync.WaitGroup{}
 	errCh := make(chan error)
 
-	server := http.NewHttpServer(errCh)
-	server.Listen(ctx, wg)
+	http.NewHttpServer(
+		[]controller.Controller{
+			video.NewCreateController(),
+			video.NewDeleteVideoController(),
+			video.NewGetVideoController(),
+			video.NewListVideoController(),
+			video.NewUpdateVideoController(),
+			audio.NewCreateController(),
+			audio.NewDeleteVideoController(),
+			audio.NewGetVideoController(),
+			audio.NewListVideoController(),
+			audio.NewUpdateVideoController(),
+		},
+		errCh,
+	).Listen(ctx, wg)
 }
