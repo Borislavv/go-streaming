bd171020bb8629f88b1bd93169eae08cad5786ad
Boris <Glazunov2142@icloud.com>
2023-09-05 10:13:06 +0400

    chore: implementation of responder interface.

--- internal/infrastructure/api/v1/response/response.go
+++ internal/infrastructure/api/v1/response/response.go
@@ -3,33 +3,40 @@ package response
 import (
 	"errors"
 	"github.com/Borislavv/video-streaming/internal/domain/errs"
-	"log"
+	"github.com/Borislavv/video-streaming/internal/domain/service"
 	"net/http"
 )
 
 const InternalServerErrorMessage = "Internal server error. Please, contact with service administrator."
 
-type Response interface {
-	Wrap() ([]byte, error)
+type Response struct {
+	logger service.Logger
 }
 
-func RespondData(w http.ResponseWriter, data any) {
+func NewResponse(logger service.Logger) *Response {
+	return &Response{logger: logger}
+}
+
+func (r *Response) RespondData(w http.ResponseWriter, data any) {
 	if _, err := w.Write(NewDataResponse(data).Wrap()); err != nil {
-		log.Fatalln(err)
+		r.logger.Emergency(err)
+		return
 	}
 }
 
-func RespondError(w http.ResponseWriter, err error) {
+func (r *Response) RespondError(w http.ResponseWriter, err error) {
 	public, ok := err.(errs.PublicError)
 	if ok {
 		w.WriteHeader(public.Status())
 		if _, err = w.Write(NewErrorResponse(public).Wrap()); err != nil {
-			log.Fatalln(err)
+			r.logger.Emergency(err)
+			return
 		}
 	} else {
 		w.WriteHeader(http.StatusInternalServerError)
 		if _, err = w.Write(NewErrorResponse(errors.New(InternalServerErrorMessage)).Wrap()); err != nil {
-			log.Fatalln(err)
+			r.logger.Emergency(err)
+			return
 		}
 	}
 }
