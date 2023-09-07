8775acdc37b4291c65891784de38dfdeda44e6b1
Boris <Glazunov2142@icloud.com>
2023-09-07 10:54:20 +0400

    feat: delete controller functionality.

--- internal/infrastructure/api/v1/controller/rest/video/delete.go
+++ internal/infrastructure/api/v1/controller/rest/video/delete.go
@@ -1,24 +1,46 @@
 package video
 
 import (
+	"github.com/Borislavv/video-streaming/internal/domain/builder"
+	"github.com/Borislavv/video-streaming/internal/domain/service"
+	"github.com/Borislavv/video-streaming/internal/infrastructure/api/v1/response"
 	"github.com/gorilla/mux"
-	"log"
 	"net/http"
 )
 
 const DeletePath = "/video/{id}"
 
 type DeleteVideoController struct {
+	builder  builder.Video
+	service  service.Video
+	response response.Responder
 }
 
-func NewDeleteVideoController() *DeleteVideoController {
-	return &DeleteVideoController{}
+func NewDeleteVideoController(
+	builder builder.Video,
+	service service.Video,
+	response response.Responder,
+) *DeleteVideoController {
+	return &DeleteVideoController{
+		builder:  builder,
+		service:  service,
+		response: response,
+	}
 }
 
 func (c *DeleteVideoController) Delete(w http.ResponseWriter, r *http.Request) {
-	if _, err := w.Write([]byte("Hello world from DELETE method!")); err != nil {
-		log.Fatalln(err)
+	videoDto, err := c.builder.BuildDeleteRequestDtoFromRequest(r)
+	if err != nil {
+		c.response.Respond(w, err)
+		return
 	}
+
+	if err = c.service.Delete(videoDto); err != nil {
+		c.response.Respond(w, err)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
 
 func (c *DeleteVideoController) AddRoute(router *mux.Router) {
