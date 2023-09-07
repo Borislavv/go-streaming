76069011a847d80e129f62000c0c438d3d744035
Boris <Glazunov2142@icloud.com>
2023-09-07 10:43:32 +0400

    feat: updated video builder and its interface.

--- internal/domain/builder/builder.go
+++ internal/domain/builder/builder.go
@@ -11,6 +11,7 @@ type Video interface {
 	BuildListRequestDtoFromRequest(r *http.Request) (*dto.VideoListRequestDto, error)
 	BuildCreateRequestDtoFromRequest(r *http.Request) (*dto.VideoCreateRequestDto, error)
 	BuildUpdateRequestDtoFromRequest(r *http.Request) (*dto.VideoUpdateRequestDto, error)
+	BuildDeleteRequestDtoFromRequest(r *http.Request) (*dto.VideoDeleteRequestDto, error)
 
 	BuildAggFromCreateRequestDto(dto dto.CreateRequest) *agg.Video
 	BuildAggFromUpdateRequestDto(dto dto.UpdateRequest) (*agg.Video, error)
--- internal/domain/builder/video.go
+++ internal/domain/builder/video.go
@@ -7,23 +7,28 @@ import (
 	"github.com/Borislavv/video-streaming/internal/domain/dto"
 	"github.com/Borislavv/video-streaming/internal/domain/entity"
 	"github.com/Borislavv/video-streaming/internal/domain/repository"
+	"github.com/Borislavv/video-streaming/internal/domain/service"
 	"github.com/Borislavv/video-streaming/internal/domain/vo"
-	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"net/http"
 	"time"
 )
 
+const (
+	ID = "id"
+)
+
 type VideoBuilder struct {
 	ctx        context.Context
+	request    service.Request
 	repository repository.Video
 }
 
-func NewVideoBuilder(ctx context.Context, repository repository.Video) *VideoBuilder {
-	return &VideoBuilder{ctx: ctx, repository: repository}
+func NewVideoBuilder(ctx context.Context, request service.Request, repository repository.Video) *VideoBuilder {
+	return &VideoBuilder{ctx: ctx, request: request, repository: repository}
 }
 
-// BuildCreateRequestDtoFromRequest - build a dto.VideoCreateRequestDto from raw http.Request
+// BuildCreateRequestDtoFromRequest - build a dto.VideoCreateRequestDto from raw *http.Request
 func (b *VideoBuilder) BuildCreateRequestDtoFromRequest(r *http.Request) (*dto.VideoCreateRequestDto, error) {
 	v := &dto.VideoCreateRequestDto{}
 	if err := json.NewDecoder(r.Body).Decode(&v); err != nil {
@@ -46,21 +51,22 @@ func (b *VideoBuilder) BuildAggFromCreateRequestDto(dto dto.CreateRequest) *agg.
 	}
 }
 
-// BuildUpdateRequestDtoFromRequest - build a dto.VideoUpdateRequestDto from raw http.Request
+// BuildUpdateRequestDtoFromRequest - build a dto.VideoUpdateRequestDto from raw *http.Request
 func (b *VideoBuilder) BuildUpdateRequestDtoFromRequest(r *http.Request) (*dto.VideoUpdateRequestDto, error) {
 	videoDto := &dto.VideoUpdateRequestDto{}
 	if err := json.NewDecoder(r.Body).Decode(&videoDto); err != nil {
 		return nil, err
 	}
 
-	hexId, found := mux.Vars(r)["id"]
-	if found {
-		oid, err := primitive.ObjectIDFromHex(hexId)
-		if err != nil {
-			return nil, err
-		}
-		videoDto.ID = vo.ID{Value: oid}
+	hexId, err := b.request.GetParameter(ID, r)
+	if err != nil {
+		return nil, err
 	}
+	oid, err := primitive.ObjectIDFromHex(hexId)
+	if err != nil {
+		return nil, err
+	}
+	videoDto.ID = vo.ID{Value: oid}
 
 	return videoDto, nil
 }
@@ -88,24 +94,37 @@ func (b *VideoBuilder) BuildAggFromUpdateRequestDto(dto dto.UpdateRequest) (*agg
 	return video, nil
 }
 
+// BuildGetRequestDtoFromRequest - build a dto.GetRequest from raw *http.Request
 func (b *VideoBuilder) BuildGetRequestDtoFromRequest(r *http.Request) (*dto.VideoGetRequestDto, error) {
 	videoDto := &dto.VideoGetRequestDto{}
 
-	hexId, found := mux.Vars(r)["id"]
-	if found {
-		oid, err := primitive.ObjectIDFromHex(hexId)
-		if err != nil {
-			return nil, err
-		}
-		videoDto.ID = vo.ID{Value: oid}
+	hexId, err := b.request.GetParameter(ID, r)
+	if err != nil {
+		return nil, err
 	}
+	oid, err := primitive.ObjectIDFromHex(hexId)
+	if err != nil {
+		return nil, err
+	}
+	videoDto.ID = vo.ID{Value: oid}
+
 	return videoDto, nil
 }
 
+// BuildListRequestDtoFromRequest - build a dto.ListRequest from raw *http.Request
 func (b *VideoBuilder) BuildListRequestDtoFromRequest(r *http.Request) (*dto.VideoListRequestDto, error) {
 	v := &dto.VideoListRequestDto{}
 	if err := json.NewDecoder(r.Body).Decode(&v); err != nil {
 		return nil, err
 	}
 	return v, nil
+}
+
+func (b *VideoBuilder) BuildDeleteRequestDtoFromRequest(r *http.Request) (*dto.VideoDeleteRequestDto, error) {
+	videoGetDto, err := b.BuildGetRequestDtoFromRequest(r)
+	if err != nil {
+		return nil, err
+	}
+
+	return &dto.VideoDeleteRequestDto{ID: videoGetDto.ID}, nil
 }
