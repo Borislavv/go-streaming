2df4785d78563e0bbe4d1515893afc1eab9d3f6b
Boris <Glazunov2142@icloud.com>
2023-09-08 16:37:32 +0400

    feat: new errs. type.

--- internal/domain/errs/validation.go
+++ internal/domain/errs/validation.go
@@ -3,15 +3,19 @@ package errs
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
-const ValidationType = "validation"
+const (
+	ValidationType   = "validation"
+	ValidationStatus = http.StatusBadRequest
+)
 
 type FieldCannotBeEmptyError Error
 
 func NewFieldCannotBeEmptyError(field string) *FieldCannotBeEmptyError {
 	return &FieldCannotBeEmptyError{
-		Message: fmt.Sprintf("field '%s' must not be empty or omitted", field),
+		Message: fmt.Sprintf("field '%v' must not be empty or omitted", field),
 		Type:    ValidationType,
 	}
 }
@@ -21,14 +25,52 @@ func (e *FieldCannotBeEmptyError) Error() string {
 }
 
 func (e *FieldCannotBeEmptyError) Status() int {
-	return http.StatusBadRequest
+	return ValidationStatus
+}
+
+type AtLeastOneFieldMustBeDefinedError Error
+
+func NewAtLeastOneFieldMustBeDefinedError(fields ...string) *AtLeastOneFieldMustBeDefinedError {
+	return &AtLeastOneFieldMustBeDefinedError{
+		Message: fmt.Sprintf("at least one of followed fields [%v] must be defined", strings.Join(fields, ", ")),
+		Type:    ValidationType,
+	}
+}
+
+func (e *AtLeastOneFieldMustBeDefinedError) Error() string {
+	return e.Message
+}
+
+func (e *AtLeastOneFieldMustBeDefinedError) Status() int {
+	return ValidationStatus
+}
+
+type FieldLengthMustBeMoreOrLessError Error
+
+func NewFieldLengthMustBeMoreOrLessError(field string, isMustBeMore bool, length int) *FieldLengthMustBeMoreOrLessError {
+	qualifier := "less"
+	if isMustBeMore {
+		qualifier = "more"
+	}
+	return &FieldLengthMustBeMoreOrLessError{
+		Message: fmt.Sprintf("length of the field '%v' must be %v than %d", field, qualifier, length),
+		Type:    ValidationType,
+	}
+}
+
+func (e *FieldLengthMustBeMoreOrLessError) Error() string {
+	return e.Message
+}
+
+func (e *FieldLengthMustBeMoreOrLessError) Status() int {
+	return ValidationStatus
 }
 
 type UniquenessCheckFailedError Error
 
 func NewUniquenessCheckFailedError(field string) *UniquenessCheckFailedError {
 	return &UniquenessCheckFailedError{
-		Message: fmt.Sprintf("uniqueness check filed due to duplicated '%s'", field),
+		Message: fmt.Sprintf("uniqueness check filed due to duplicated '%v'", field),
 		Type:    ValidationType,
 	}
 }
@@ -38,5 +80,5 @@ func (e *UniquenessCheckFailedError) Error() string {
 }
 
 func (e *UniquenessCheckFailedError) Status() int {
-	return http.StatusBadRequest
+	return ValidationStatus
 }
