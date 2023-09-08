ffba1a07d0f00d417bd1c95cee3d0853b08e2686
Boris <Glazunov2142@icloud.com>
2023-09-08 16:39:38 +0400

    chore: implemented video list controller.

--- internal/infrastructure/api/v1/controller/rest/video/list.go
+++ internal/infrastructure/api/v1/controller/rest/video/list.go
@@ -1,29 +1,52 @@
 package video
 
 import (
+	"github.com/Borislavv/video-streaming/internal/domain/builder"
+	"github.com/Borislavv/video-streaming/internal/domain/service"
+	"github.com/Borislavv/video-streaming/internal/infrastructure/api/v1/response"
 	"github.com/gorilla/mux"
-	"log"
 	"net/http"
 )
 
 const ListPath = "/video"
 
 type ListVideoController struct {
+	builder  builder.Video
+	service  service.Video
+	response response.Responder
 }
 
-func NewListVideoController() *ListVideoController {
-	return &ListVideoController{}
-}
-
-func (l *ListVideoController) List(w http.ResponseWriter, r *http.Request) {
-	if _, err := w.Write([]byte("Hello world from GET:list method!")); err != nil {
-		log.Fatalln(err)
+func NewListVideoController(
+	builder builder.Video,
+	service service.Video,
+	response response.Responder,
+) *ListVideoController {
+	return &ListVideoController{
+		builder:  builder,
+		service:  service,
+		response: response,
 	}
 }
 
-func (l *ListVideoController) AddRoute(router *mux.Router) {
+func (c *ListVideoController) List(w http.ResponseWriter, r *http.Request) {
+	reqDto, err := c.builder.BuildListRequestDtoFromRequest(r)
+	if err != nil {
+		c.response.Respond(w, err)
+		return
+	}
+
+	videos, err := c.service.List(reqDto)
+	if err != nil {
+		c.response.Respond(w, err)
+		return
+	}
+
+	c.response.Respond(w, videos)
+}
+
+func (c *ListVideoController) AddRoute(router *mux.Router) {
 	router.
 		Path(ListPath).
-		HandlerFunc(l.List).
+		HandlerFunc(c.List).
 		Methods(http.MethodGet)
 }
