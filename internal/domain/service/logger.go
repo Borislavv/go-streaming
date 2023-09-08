1e0f673f9797c424fad47178a2e9a44410040d5e
Boris <Glazunov2142@icloud.com>
2023-09-08 16:42:09 +0400

    chore: todo for logger. #tech_debt

--- internal/domain/service/logger.go
+++ internal/domain/service/logger.go
@@ -1,5 +1,6 @@
 package service
 
+// TODO - logger must be improved for write 3-5 or all last stack frames due to handle errors properly
 type Logger interface {
 	Info(msg string)
 	Debug(msg string)
