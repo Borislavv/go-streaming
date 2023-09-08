d4aa26251d08f6901f65188cfd4a00d71db1b18f
Boris <Glazunov2142@icloud.com>
2023-09-08 16:38:53 +0400

    feat: improved request params. extractor due to handle query params.

--- internal/infrastructure/api/v1/request/request.go
+++ internal/infrastructure/api/v1/request/request.go
@@ -1,6 +1,7 @@
 package request
 
 import (
+	"errors"
 	"github.com/Borislavv/video-streaming/internal/domain/errs"
 	"github.com/gorilla/mux"
 	"net/http"
@@ -21,11 +22,23 @@ func NewParametersExtractor() *ParametersExtractor {
 
 // Parameters - will return a map with param. names as keys to values
 func (e *ParametersExtractor) Parameters(req *http.Request) map[string]string {
-	return mux.Vars(req)
+	params := make(map[string]string)
+
+	for name, param := range req.URL.Query() {
+		params[name] = param[0]
+	}
+	for name, param := range mux.Vars(req) {
+		params[name] = param
+	}
+
+	return params
 }
 
 // HasParameter - checking the param. is existing in request
 func (e *ParametersExtractor) HasParameter(param string, req *http.Request) bool {
+	if param == "" {
+		return false
+	}
 	if _, ok := e.Parameters(req)[param]; ok {
 		return true
 	}
@@ -34,8 +47,11 @@ func (e *ParametersExtractor) HasParameter(param string, req *http.Request) bool
 
 // GetParameter - checking if the param. is existing in request, it will be returned
 func (e *ParametersExtractor) GetParameter(param string, req *http.Request) (string, error) {
+	if param == "" {
+		return "", errs.NewFieldCannotBeEmptyError(param)
+	}
 	if v, ok := e.Parameters(req)[param]; ok {
 		return v, nil
 	}
-	return "", errs.NewFieldCannotBeEmptyError(param)
+	return "", errors.New("parameter '" + param + "' not found into query or path")
 }
