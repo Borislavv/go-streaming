764c8ac69475217fe94d170300d9193a4eccf951
Boris <Glazunov2142@icloud.com>
2023-09-09 14:37:26 +0400

    feat: improved logger, implemented abstractlogger.

--- internal/infrastructure/logger/clilogger.go
+++ internal/infrastructure/logger/clilogger.go
@@ -1,127 +1,285 @@
 package logger
 
 import (
-	"github.com/Borislavv/video-streaming/internal/infrastructure/logger/errtype"
 	"log"
-	"runtime"
 )
 
 type CliLogger struct {
-	errCh chan error
+	abstractLogger
+	errCh chan introspectedError
 }
 
-func NewCliLogger(errCh chan error) *CliLogger {
+func NewCliLogger(buffer int) (logger *CliLogger, close func()) {
 	l := &CliLogger{
-		errCh: errCh,
+		errCh: make(chan introspectedError, buffer),
 	}
-
 	l.handle()
-
-	return l
+	return l, l.Close()
 }
 
-func (l *CliLogger) Info(msg string) {
-	file, function, line := l.trace()
-	l.errCh <- errtype.InfoLevel{
-		Message:  msg,
-		File:     file,
-		Function: function,
-		Line:     line,
+func (l *CliLogger) Close() func() {
+	return func() {
+		close(l.errCh)
 	}
 }
 
-func (l *CliLogger) Debug(msg string) {
+func (l *CliLogger) Log(err error) {
 	file, function, line := l.trace()
-	l.errCh <- errtype.DebugLevel{
-		Message:  msg,
-		File:     file,
-		Function: function,
-		Line:     line,
+	l.log(err, file, function, line)
+}
+
+func (l *CliLogger) LogPropagate(err error) error {
+	file, function, line := l.trace()
+	l.log(err, file, function, line)
+	return err
+}
+
+func (l *CliLogger) Info(strOrErr any) {
+	file, function, line := l.trace()
+
+	err := l.error(strOrErr)
+
+	l.errCh <- infoLevelError{
+		Er: err,
+		Fl: file,
+		Fn: function,
+		Ln: line,
 	}
 }
 
-func (l *CliLogger) Warning(msg string) {
+func (l *CliLogger) InfoPropagate(strOrErr any) error {
 	file, function, line := l.trace()
-	l.errCh <- errtype.WarningLevel{
-		Message:  msg,
-		File:     file,
-		Function: function,
-		Line:     line,
+
+	err := l.error(strOrErr)
+
+	l.errCh <- infoLevelError{
+		Er: err,
+		Fl: file,
+		Fn: function,
+		Ln: line,
+	}
+
+	return err
+}
+
+func (l *CliLogger) Debug(strOrErr any) {
+	file, function, line := l.trace()
+
+	err := l.error(strOrErr)
+
+	l.errCh <- debugLevelError{
+		Er: err,
+		Fl: file,
+		Fn: function,
+		Ln: line,
 	}
 }
 
-func (l *CliLogger) Error(e error) {
+func (l *CliLogger) DebugPropagate(strOrErr any) error {
 	file, function, line := l.trace()
-	l.errCh <- errtype.ErrorLevel{
-		Err:      e,
-		File:     file,
-		Function: function,
-		Line:     line,
+
+	err := l.error(strOrErr)
+
+	l.errCh <- debugLevelError{
+		Er: err,
+		Fl: file,
+		Fn: function,
+		Ln: line,
+	}
+
+	return err
+}
+
+func (l *CliLogger) Warning(strOrErr any) {
+	file, function, line := l.trace()
+
+	err := l.error(strOrErr)
+
+	l.errCh <- warningLevelError{
+		Er: err,
+		Fl: file,
+		Fn: function,
+		Ln: line,
 	}
 }
 
-func (l *CliLogger) Critical(e error) {
+func (l *CliLogger) WarningPropagate(strOrErr any) error {
 	file, function, line := l.trace()
-	l.errCh <- errtype.CriticalLevel{
-		Err:      e,
-		File:     file,
-		Function: function,
-		Line:     line,
+
+	err := l.error(strOrErr)
+
+	l.errCh <- warningLevelError{
+		Er: err,
+		Fl: file,
+		Fn: function,
+		Ln: line,
+	}
+
+	return err
+}
+
+func (l *CliLogger) Error(strOrErr any) {
+	file, function, line := l.trace()
+
+	err := l.error(strOrErr)
+
+	l.errCh <- errorLevelError{
+		Er: err,
+		Fl: file,
+		Fn: function,
+		Ln: line,
 	}
 }
 
-func (l *CliLogger) Emergency(e error) {
+func (l *CliLogger) ErrorPropagate(strOrErr any) error {
 	file, function, line := l.trace()
-	l.errCh <- errtype.EmergencyLevel{
-		Err:      e,
-		File:     file,
-		Function: function,
-		Line:     line,
+
+	err := l.error(strOrErr)
+
+	l.errCh <- errorLevelError{
+		Er: err,
+		Fl: file,
+		Fn: function,
+		Ln: line,
+	}
+
+	return err
+}
+
+func (l *CliLogger) Critical(strOrErr any) {
+	file, function, line := l.trace()
+
+	err := l.error(strOrErr)
+
+	l.errCh <- criticalLevelError{
+		Er: err,
+		Fl: file,
+		Fn: function,
+		Ln: line,
 	}
 }
 
+func (l *CliLogger) CriticalPropagate(strOrErr any) error {
+	file, function, line := l.trace()
+
+	err := l.error(strOrErr)
+
+	l.errCh <- criticalLevelError{
+		Er: err,
+		Fl: file,
+		Fn: function,
+		Ln: line,
+	}
+
+	return err
+}
+
+func (l *CliLogger) Emergency(strOrErr any) {
+	file, function, line := l.trace()
+
+	err := l.error(strOrErr)
+
+	l.errCh <- emergencyLevelError{
+		Er: err,
+		Fl: file,
+		Fn: function,
+		Ln: line,
+	}
+}
+
+func (l *CliLogger) EmergencyPropagate(strOrErr any) error {
+	file, function, line := l.trace()
+
+	err := l.error(strOrErr)
+
+	l.errCh <- emergencyLevelError{
+		Er: err,
+		Fl: file,
+		Fn: function,
+		Ln: line,
+	}
+
+	return err
+}
+
+// TODO must be injected writer for write logs through it
 func (l *CliLogger) handle() {
 	go func() {
-		for er := range l.errCh {
-			t, f, fn, ln := l.details(er)
-			log.Printf("\n\t[%s]\n\t\tFile: %s:%d\n\t\tFunc: %s\n\t\tMessage: %s\n", t, f, ln, fn, er)
+		for err := range l.errCh {
+			log.Printf(
+				"\n\t[%v]\n\t\tFile: %v:%d\n\t\tFunc: %v\n\t\tMessage: %v\n",
+				l.toReadableLevel(err),
+				err.File(),
+				err.Line(),
+				err.Func(),
+				err.Error(),
+			)
 		}
 	}()
 }
 
-func (l *CliLogger) trace() (string, string, int) {
-	pc := make([]uintptr, 15)
-	n := runtime.Callers(3, pc)
-	frames := runtime.CallersFrames(pc[:n])
-	frame, _ := frames.Next()
-
-	return frame.File, frame.Func.Name(), frame.Line
-}
-
-func (l *CliLogger) details(e error) (t string, f string, fn string, ln int) {
-	if iErr, iOk := e.(errtype.InfoLevel); iOk {
-		return iErr.Type(), iErr.File, iErr.Function, iErr.Line
+func (l *CliLogger) log(e error, file string, function string, line int) {
+	err, isLoggableErr := e.(LoggableError)
+	if !isLoggableErr {
+		l.errCh <- errorLevelError{
+			Er: e,
+			Fl: file,
+			Fn: function,
+			Ln: line,
+		}
+		return
 	}
 
-	if dErr, dOk := e.(errtype.DebugLevel); dOk {
-		return dErr.Type(), dErr.File, dErr.Function, dErr.Line
+	switch err.Level() {
+	case InfoLevel:
+		l.errCh <- infoLevelError{
+			Er: err,
+			Fl: file,
+			Fn: function,
+			Ln: line,
+		}
+		return
+	case DebugLevel:
+		l.errCh <- infoLevelError{
+			Er: err,
+			Fl: file,
+			Fn: function,
+			Ln: line,
+		}
+		return
+	case WarningLevel:
+		l.errCh <- warningLevelError{
+			Er: err,
+			Fl: file,
+			Fn: function,
+			Ln: line,
+		}
+		return
+	case ErrorLevel:
+		l.errCh <- errorLevelError{
+			Er: err,
+			Fl: file,
+			Fn: function,
+			Ln: line,
+		}
+		return
+	case CriticalLevel:
+		l.errCh <- criticalLevelError{
+			Er: err,
+			Fl: file,
+			Fn: function,
+			Ln: line,
+		}
+		return
+	case EmergencyLevel:
+		l.errCh <- emergencyLevelError{
+			Er: err,
+			Fl: file,
+			Fn: function,
+			Ln: line,
+		}
+		return
 	}
 
-	if wErr, wOk := e.(errtype.WarningLevel); wOk {
-		return wErr.Type(), wErr.File, wErr.Function, wErr.Line
-	}
-
-	if eErr, eOk := e.(errtype.ErrorLevel); eOk {
-		return eErr.Type(), eErr.File, eErr.Function, eErr.Line
-	}
-
-	if cErr, cOk := e.(errtype.CriticalLevel); cOk {
-		return cErr.Type(), cErr.File, cErr.Function, cErr.Line
-	}
-
-	if emErr, emOk := e.(errtype.EmergencyLevel); emOk {
-		return emErr.Type(), emErr.File, emErr.Function, emErr.Line
-	}
-
-	return "unknown error type", "", "", 0
+	panic("clilogger.log(): undefined error level received")
 }
