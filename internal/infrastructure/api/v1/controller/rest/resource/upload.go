f9197e5d01cd205dc885f27bb2fbde7d8ba1e4e3
Boris <Glazunov2142@icloud.com>
2023-09-10 14:06:44 +0400

    feat: resolved sketch of resource uplaod controller. #todo
    TODO: need to be refactored to use the resource.Upload service for to do the same.

--- internal/infrastructure/api/v1/controller/rest/resource/upload.go
+++ internal/infrastructure/api/v1/controller/rest/resource/upload.go
@@ -1,21 +1,36 @@
 package resource
 
 import (
+	"github.com/Borislavv/video-streaming/internal/domain/service"
+	"github.com/Borislavv/video-streaming/internal/infrastructure/api/v1/response"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
-const UploadPath = "/upload"
+const UploadPath = "/resource"
 
 type UploadResourceController struct {
+	uploader service.Uploader
+	responer response.Responder
 }
 
-func NewUploadResourceController() *UploadResourceController {
-	return &UploadResourceController{}
+func NewUploadResourceController(
+	uploader service.Uploader,
+	responer response.Responder,
+) *UploadResourceController {
+	return &UploadResourceController{
+		uploader: uploader,
+		responer: responer,
+	}
 }
 
 func (c *UploadResourceController) Upload(w http.ResponseWriter, r *http.Request) {
-
+	id, err := c.uploader.Upload(r)
+	if err != nil {
+		c.responer.Respond(w, err)
+		return
+	}
+	c.responer.Respond(w, id)
 }
 
 func (c *UploadResourceController) AddRoute(router *mux.Router) {
