67caef35132e1298b4df12f748b161859729dfcd
Boris <Glazunov2142@icloud.com>
2023-09-11 17:48:20 +0400

    chore: commented storage interface.

--- internal/domain/service/storage.go
+++ internal/domain/service/storage.go
@@ -6,6 +6,7 @@ import (
 
 type Storage interface {
 	// Has is checking whether the file already exists.
-	Has(header *multipart.FileHeader) (has bool, e error)
-	Store(file multipart.File, header *multipart.FileHeader) (id string, e error)
+	Has(header *multipart.FileHeader) (has bool, err error)
+	// Store is saving file and calculating new hashed name.
+	Store(file multipart.File, header *multipart.FileHeader) (filename string, filepath string, err error)
 }
