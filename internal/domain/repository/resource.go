51f35556d2f736bb9f4fd7f89701e65e961d37dd
Boris <Glazunov2142@icloud.com>
2023-09-12 09:23:22 +0400

    chore: separated Resource interface.

---
+++ internal/domain/repository/resource.go
@@ -0,0 +1,12 @@
+package repository
+
+import (
+	"context"
+	"github.com/Borislavv/video-streaming/internal/domain/agg"
+	"github.com/Borislavv/video-streaming/internal/domain/vo"
+)
+
+type Resource interface {
+	Find(ctx context.Context, id vo.ID) (*agg.Resource, error)
+	Insert(ctx context.Context, resource *agg.Resource) (*agg.Resource, error)
+}
