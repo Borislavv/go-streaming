6bd9c517c6e19245f63d01c53af4af905f6813d4
Boris <Glazunov2142@icloud.com>
2023-09-12 09:10:58 +0400

    chore: streamer and reader moved in the infrastructure layer.

---
+++ internal/infrastructure/service/reader/reader.go
@@ -0,0 +1,79 @@
+package reader
+
+import (
+	"fmt"
+	"github.com/Borislavv/video-streaming/internal/domain/dto"
+	"github.com/Borislavv/video-streaming/internal/domain/logger"
+	"io"
+	"os"
+)
+
+// ChunkSize is 2.5MB
+const ChunkSize = 1024 * 1024 * 2.5
+
+type ResourceReader struct {
+	logger logger.Logger
+}
+
+func NewReaderService(logger logger.Logger) *ResourceReader {
+	return &ResourceReader{logger: logger}
+}
+
+func (r *ResourceReader) Read(resource dto.Resource) chan *dto.Chunk {
+	r.logger.Info("[reader]: reading started")
+
+	chunksCh := make(chan *dto.Chunk, 1)
+	go r.handleRead(resource, chunksCh)
+
+	return chunksCh
+}
+
+func (r *ResourceReader) handleRead(resource dto.Resource, chunksCh chan *dto.Chunk) {
+	defer r.logger.Info("[reader]: reading stopped")
+	defer close(chunksCh)
+
+	file, err := os.Open(resource.GetFilepath())
+	if err != nil {
+		r.logger.Error(err)
+		return
+	}
+	defer func() {
+		if err = file.Close(); err != nil {
+			r.logger.Error(err)
+			return
+		}
+	}()
+
+	for {
+		chunk := dto.NewChunk(ChunkSize)
+
+		chunk.Len, err = file.Read(chunk.Data)
+		if err != nil {
+			if err == io.EOF {
+				r.logger.Info("[reader]: file was successfully reader")
+				break
+			}
+			r.logger.Error(err)
+			return
+		}
+
+		r.sendChunk(chunk, chunksCh)
+	}
+}
+
+func (r *ResourceReader) sendChunk(chunk *dto.Chunk, chunksCh chan *dto.Chunk) {
+	if chunk.Len == 0 {
+		return
+	}
+
+	if chunk.Len < ChunkSize {
+		lastChunk := make([]byte, chunk.Len)
+		lastChunk = chunk.Data[:chunk.Len]
+		chunk.Data = lastChunk
+	}
+
+	if chunk.Len > 0 {
+		r.logger.Info(fmt.Sprintf("[reader]: reader %d bytes", chunk.Len))
+		chunksCh <- chunk
+	}
+}
---
+++ internal/infrastructure/service/streamer/streamer.go
@@ -0,0 +1,220 @@
+package streamer
+
+import (
+	"context"
+	"fmt"
+	"github.com/Borislavv/video-streaming/internal/domain/agg"
+	"github.com/Borislavv/video-streaming/internal/domain/dto"
+	"github.com/Borislavv/video-streaming/internal/domain/entity"
+	"github.com/Borislavv/video-streaming/internal/domain/logger"
+	"github.com/Borislavv/video-streaming/internal/domain/repository"
+	"github.com/Borislavv/video-streaming/internal/domain/service"
+	"github.com/gorilla/websocket"
+	"gopkg.in/vansante/go-ffprobe.v2"
+	"os"
+	"sync"
+)
+
+const VideoPath = "/streaming_root/internal/infrastructure/static/tmp/video/example_video_new.mp4"
+const Video2Path = "/streaming_root/internal/infrastructure/static/tmp/video/example_video_2_new.mp4"
+const Video3Path = "/streaming_root/internal/infrastructure/static/tmp/video/example_video_3_new.mp4"
+
+type Action string
+
+func (a Action) String() string {
+	return string(a)
+}
+
+const (
+	Start             Action = "start"
+	Stop              Action = "stop"
+	Next              Action = "next"
+	Previous          Action = "prev"
+	DecreaseBufferCap Action = "decrBuff"
+)
+
+type ResourceStreamer struct {
+	ctx             context.Context
+	logger          logger.Logger
+	reader          service.Reader
+	videoRepository repository.Video
+}
+
+func NewStreamingService(
+	ctx context.Context,
+	logger logger.Logger,
+	reader service.Reader,
+	videoRepository repository.Video,
+) *ResourceStreamer {
+	return &ResourceStreamer{
+		ctx:             ctx,
+		reader:          reader,
+		logger:          logger,
+		videoRepository: videoRepository,
+	}
+}
+
+func (s *ResourceStreamer) Stream(conn *websocket.Conn) {
+	s.logger.Info("[streamer]: start streaming")
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	actionCh := make(chan Action, 1)
+	decrBuffCapCh := make(chan struct{})
+
+	go s.handleMessages(conn, actionCh, decrBuffCapCh)
+	go s.handleBufferSize(decrBuffCapCh)
+	go s.handleStream(wg, conn, actionCh)
+
+	wg.Wait()
+
+	s.logger.Info("[streamer]: streaming is stopped")
+}
+
+func (s *ResourceStreamer) handleStream(wg *sync.WaitGroup, conn *websocket.Conn, actionCh <-chan Action) {
+	defer wg.Done()
+	defer s.logger.Info("handleStream: exit")
+
+	videos, err := s.videoRepository.FindList(
+		s.ctx,
+		&dto.VideoListRequestDto{
+			PaginationRequestDto: dto.PaginationRequestDto{
+				Page:  1,
+				Limit: 10,
+			},
+		},
+	)
+	if err != nil {
+		s.logger.Log(err)
+		return
+	}
+
+	l := len(videos) - 1
+	c := 0
+	for action := range actionCh {
+		if action == Start {
+			s.logger.Info("action 'start' received")
+			v := videos[c]
+			s.logger.Info(fmt.Sprintf("streaming 'video':'%v'", v.Name))
+			s.stream(v, conn)
+		} else if action == Next {
+			if c < l {
+				c++
+			}
+			s.logger.Info("action 'next' received")
+			v := videos[c]
+			s.logger.Info(fmt.Sprintf("streaming 'video':'%v'", v.Name))
+			s.stream(v, conn)
+		} else if action == Previous {
+			if c >= 1 {
+				c--
+			}
+			s.logger.Info("action 'previous' received")
+			v := videos[c]
+			s.logger.Info(fmt.Sprintf("streaming 'video':'%v'", v.Name))
+			s.stream(v, conn)
+		}
+	}
+}
+
+func (s *ResourceStreamer) stream(video *agg.Video, conn *websocket.Conn) {
+	startMsg := Start.String()
+
+	ac, vc := s.codecs(video.Resource)
+	if ac != nil {
+		startMsg += ":" + ac.CodecTagString
+	} else {
+		startMsg += ":"
+	}
+	if vc != nil {
+		startMsg += ":" + vc.CodecTagString
+	} else {
+		startMsg += ":"
+	}
+
+	if err := conn.WriteMessage(websocket.TextMessage, []byte(startMsg)); err != nil {
+		s.logger.Error(err)
+		return
+	}
+
+	for chunk := range s.reader.Read(video.Resource) {
+		if chunk.Err != nil {
+			s.logger.Critical(chunk.Err)
+			continue
+		}
+
+		if err := conn.WriteMessage(websocket.BinaryMessage, chunk.Data); err != nil {
+			s.logger.Error(err)
+			continue
+		}
+
+		//s.logger.Info(fmt.Sprintf("[streamer]: wrote %d bytes to websocket", chunk.Len))
+	}
+
+	if err := conn.WriteMessage(websocket.TextMessage, []byte(Stop.String())); err != nil {
+		s.logger.Error(err)
+		return
+	}
+}
+
+func (s *ResourceStreamer) handleBufferSize(decrBuffCapCh <-chan struct{}) {
+	defer s.logger.Info("handleBufferSize: exit")
+
+	for range decrBuffCapCh {
+		s.logger.Info("Decreased buffer capacity")
+	}
+}
+
+func (s *ResourceStreamer) handleMessages(
+	conn *websocket.Conn,
+	actionsCh chan<- Action,
+	decrBuffCapCh chan<- struct{},
+) {
+	defer close(actionsCh)
+	defer close(decrBuffCapCh)
+	defer s.logger.Info("handleMessages: exit")
+
+	for {
+		t, b, err := conn.ReadMessage()
+		if err != nil {
+			if websocket.IsCloseError(err, websocket.CloseGoingAway, websocket.CloseNormalClosure) {
+				return
+			}
+			s.logger.Error(err)
+			return
+		}
+		if t == websocket.TextMessage {
+			action := Action(b)
+
+			if action == DecreaseBufferCap {
+				decrBuffCapCh <- struct{}{}
+				continue
+			}
+			if action == Start || action == Stop || action == Next || action == Previous {
+				actionsCh <- action
+				continue
+			}
+			s.logger.Emergency(fmt.Sprintf("found unknown action: %s", action))
+		}
+	}
+}
+
+func (s *ResourceStreamer) codecs(resource entity.Resource) (audioStream *ffprobe.Stream, videoStream *ffprobe.Stream) {
+	file, err := os.Open(resource.GetFilepath())
+	if err != nil {
+		s.logger.Emergency(err)
+	}
+	defer func() {
+		if err = file.Close(); err != nil {
+			s.logger.Emergency(err)
+		}
+	}()
+
+	data, err := ffprobe.ProbeReader(context.Background(), file)
+	if err != nil {
+		s.logger.Emergency(err)
+	}
+
+	return data.FirstAudioStream(), data.FirstVideoStream()
+}
