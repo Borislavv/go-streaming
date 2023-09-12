f0280633396e71ef69f1510fccc5ede8b8b89c7c
Boris <Glazunov2142@icloud.com>
2023-09-12 09:25:03 +0400

    feat: moved Reader and Streamer interfaces.

--- internal/domain/service/service.go
+++ internal/domain/service/service.go
@@ -3,7 +3,6 @@ package service
 import (
 	"github.com/Borislavv/video-streaming/internal/domain/agg"
 	"github.com/Borislavv/video-streaming/internal/domain/dto"
-	"github.com/gorilla/websocket"
 )
 
 type Resource interface {
@@ -16,12 +15,4 @@ type Video interface {
 	Create(req dto.CreateRequest) (*agg.Video, error)
 	Update(req dto.UpdateRequest) (*agg.Video, error)
 	Delete(req dto.DeleteRequest) error
-}
-
-type Reader interface {
-	Read(resource dto.Resource) chan *dto.Chunk
-}
-
-type Streamer interface {
-	Stream(conn *websocket.Conn)
 }
--- internal/domain/service/uploader.go
+++ internal/domain/service/uploader.go
@@ -1,7 +1,10 @@
 package service
 
-import "net/http"
+import (
+	"github.com/Borislavv/video-streaming/internal/domain/dto"
+)
 
 type Uploader interface {
-	Upload(r *http.Request) (resourceId string, err error)
+	// Upload method will be store a file on a disk and calculate a new hashed name. Request DTO mutation!
+	Upload(req dto.UploadRequest) (err error)
 }
