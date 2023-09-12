7a0237c79f9627ad5bc936bc5b913219b0e84aae
Boris <Glazunov2142@icloud.com>
2023-09-12 09:09:42 +0400

    feat: implemented marshaler and unmarshaler interfaces for id value object.

--- internal/domain/vo/id.go
+++ internal/domain/vo/id.go
@@ -1,9 +1,27 @@
 package vo
 
 import (
+	"fmt"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"strings"
 )
 
 type ID struct {
 	Value primitive.ObjectID `json:"value" bson:"_id,omitempty"`
+}
+
+func (id *ID) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf("\"%v\"", id.Value.Hex())), nil
+}
+
+func (id *ID) UnmarshalJSON(b []byte) error {
+	hex := strings.ReplaceAll(string(b), "\"", "")
+
+	oid, err := primitive.ObjectIDFromHex(hex)
+	if err != nil {
+		return err
+	}
+	id.Value = oid
+
+	return nil
 }
