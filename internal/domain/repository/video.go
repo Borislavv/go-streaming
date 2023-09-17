0842d35e17d2993063d0ec7e75add511d43125e3
Boris <Glazunov2142@icloud.com>
2023-09-17 07:37:18 +0400

    chore: video repo. commented and added todo.

--- internal/domain/repository/video.go
+++ internal/domain/repository/video.go
@@ -9,10 +9,13 @@ import (
 
 type Video interface {
 	Find(ctx context.Context, id vo.ID) (*agg.Video, error)
+
+	// todo must be refactored to query builder
 	FindByResource(ctx context.Context, resource *agg.Resource) (*agg.Video, error)
 	FindList(ctx context.Context, query dto.ListRequest) ([]*agg.Video, error)
+	Has(ctx context.Context, video *agg.Video) (bool, error)
+
 	Insert(ctx context.Context, video *agg.Video) (*agg.Video, error)
 	Update(ctx context.Context, video *agg.Video) (*agg.Video, error)
-	Has(ctx context.Context, video *agg.Video) (bool, error)
 	Remove(ctx context.Context, video *agg.Video) error
 }
