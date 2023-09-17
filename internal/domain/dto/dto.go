fd2eac1a6aeca0de1c593842c3f31bf72d343a35
Boris <Glazunov2142@icloud.com>
2023-09-17 07:38:14 +0400

    chore: moved chunk.

--- internal/domain/dto/dto.go
+++ internal/domain/dto/dto.go
@@ -45,20 +45,3 @@ type PaginatedRequest interface {
 	GetPage() int
 	GetLimit() int
 }
-
-type Chunked interface {
-	GetLen() int
-	SetLen(len int)
-	GetData() []byte
-	SetData(data []byte)
-	GetError() error
-	SetError(err error)
-}
-
-type Resource interface {
-	GetFilepath() string
-	//GetName() string
-	//GetFilename() string
-	//GetFilesize() int64
-	//GetFileMIME() textproto.MIMEHeader
-}
