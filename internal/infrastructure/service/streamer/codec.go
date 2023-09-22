e9524930b5b422ed5416e0b54f5b2a492cbbb6c4
Boris <Glazunov2142@icloud.com>
2023-09-22 17:40:13 +0300

    feat: new struct ResourceCodecs in the streaming package.

---
+++ internal/infrastructure/service/streamer/codec.go
@@ -0,0 +1,52 @@
+package streamer
+
+import (
+	"context"
+	"github.com/Borislavv/video-streaming/internal/domain/entity"
+	"github.com/Borislavv/video-streaming/internal/domain/logger"
+	"gopkg.in/vansante/go-ffprobe.v2"
+	"os"
+)
+
+type ResourceCodecs struct {
+	ctx    context.Context
+	logger logger.Logger
+}
+
+func NewResourceCodecs(ctx context.Context, logger logger.Logger) *ResourceCodecs {
+	return &ResourceCodecs{
+		ctx:    ctx,
+		logger: logger,
+	}
+}
+
+// Determine will determine video and audio stream codecs of target resource
+func (d *ResourceCodecs) Determine(
+	resource entity.Resource,
+) (
+	audioCodec string,
+	videoCodec string,
+	e error,
+) {
+	file, err := os.Open(resource.GetFilepath())
+	if err != nil {
+		return "", "", d.logger.LogPropagate(err)
+	}
+	defer func() { _ = file.Close() }()
+
+	data, err := ffprobe.ProbeReader(d.ctx, file)
+	if err != nil {
+		return "", "", d.logger.LogPropagate(err)
+	}
+
+	audioCodec = ""
+	videoCodec = ""
+	if data.FirstAudioStream() != nil {
+		audioCodec = data.FirstAudioStream().CodecTagString
+	}
+	if data.FirstVideoStream() != nil {
+		videoCodec = data.FirstVideoStream().CodecTagString
+	}
+
+	return audioCodec, videoCodec, nil
+}
