6800418384f6ca4b617d8a2636d4ffc7cacb8705
Boris <Glazunov2142@icloud.com>
2023-09-24 10:52:03 +0300

    feat: proto communicator moved to itself own package.

--- internal/infrastructure/service/streamer/proto/ws/communicator.go
+++ internal/infrastructure/service/streamer/proto/ws/communicator.go
@@ -1,9 +1,10 @@
-package streamer
+package ws
 
 import (
 	"fmt"
 	"github.com/Borislavv/video-streaming/internal/domain/dto"
 	"github.com/Borislavv/video-streaming/internal/domain/logger"
+	"github.com/Borislavv/video-streaming/internal/infrastructure/service/streamer/action/enum"
 	"github.com/gorilla/websocket"
 	"strings"
 )
@@ -15,32 +16,19 @@ const (
 	startMsgPref string = "start"
 	errMsgPref   string = "error"
 	stopMsgPref  string = "stop"
-
-	streamByID ActionEnum = "ID"
 )
 
-type Action struct {
-	do   ActionEnum
-	data string
-}
-
-type ActionEnum string
-
-func (a ActionEnum) String() string {
-	return string(a)
-}
-
-type WebSocketCommunicator struct {
+type Communicator struct {
 	logger logger.Logger
 }
 
-func NewWebSocketCommunicator(logger logger.Logger) *WebSocketCommunicator {
-	return &WebSocketCommunicator{
+func NewWebSocketCommunicator(logger logger.Logger) *Communicator {
+	return &Communicator{
 		logger: logger,
 	}
 }
 
-func (w *WebSocketCommunicator) Start(audioCodec string, videoCodec string, conn *websocket.Conn) error {
+func (w *Communicator) Start(audioCodec string, videoCodec string, conn *websocket.Conn) error {
 	b := strings.Builder{}
 	b.WriteString(startMsgPref)
 	b.WriteString(protoSeparator)
@@ -57,7 +45,7 @@ func (w *WebSocketCommunicator) Start(audioCodec string, videoCodec string, conn
 	return nil
 }
 
-func (w *WebSocketCommunicator) Send(chunk dto.Chunk, conn *websocket.Conn) error {
+func (w *Communicator) Send(chunk dto.Chunk, conn *websocket.Conn) error {
 	if chunk.GetError() != nil {
 		return w.logger.CriticalPropagate(fmt.Sprintf("[%v]: %v", conn.RemoteAddr(), chunk.GetError().Error()))
 	}
@@ -69,15 +57,15 @@ func (w *WebSocketCommunicator) Send(chunk dto.Chunk, conn *websocket.Conn) erro
 	return nil
 }
 
-func (w *WebSocketCommunicator) Parse(bytes []byte) (action ActionEnum, data string) {
+func (w *Communicator) Parse(bytes []byte) (action enum.Actions, data string) {
 	p := strings.Split(string(bytes), protoSeparator)
 	if len(p) > 1 {
-		return ActionEnum(p[0]), p[1]
+		return enum.Actions(p[0]), p[1]
 	}
-	return ActionEnum(p[0]), ""
+	return enum.Actions(p[0]), ""
 }
 
-func (w *WebSocketCommunicator) Error(err error, conn *websocket.Conn) error {
+func (w *Communicator) Error(err error, conn *websocket.Conn) error {
 	msg := []byte(fmt.Sprintf("%v:%v", errMsgPref, err.Error()))
 
 	if e := conn.WriteMessage(websocket.TextMessage, msg); e != nil {
@@ -87,7 +75,7 @@ func (w *WebSocketCommunicator) Error(err error, conn *websocket.Conn) error {
 	return nil
 }
 
-func (w *WebSocketCommunicator) Stop(conn *websocket.Conn) error {
+func (w *Communicator) Stop(conn *websocket.Conn) error {
 	if err := conn.WriteMessage(websocket.TextMessage, []byte(stopMsgPref)); err != nil {
 		return w.logger.CriticalPropagate(fmt.Sprintf("[%v]: %v", conn.RemoteAddr(), err.Error()))
 	}
