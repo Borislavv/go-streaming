ac46e34ed77a596bd1db5840abb61647eabd147b
Boris <Glazunov2142@icloud.com>
2023-09-28 17:44:56 +0300

    feat: new filereaderinterface.

--- internal/infrastructure/service/reader/reader.go
+++ internal/infrastructure/service/reader/reader.go
@@ -1,67 +1,80 @@
 package reader
 
 import (
+	"context"
 	"fmt"
-	"github.com/Borislavv/video-streaming/internal/domain/dto"
 	"github.com/Borislavv/video-streaming/internal/domain/logger"
-	"io"
+	"github.com/Borislavv/video-streaming/internal/infrastructure/service/reader/model"
 	"os"
 )
 
-const (
-	ChunkSize      = 1024 * 1024 * 1 // 1MB
-	ChunksChBuffer = 1
-)
+const chunksChBuffer = 1
 
-type ResourceReader struct {
-	logger logger.Logger
+type FileReaderService struct {
+	ctx       context.Context
+	logger    logger.Logger
+	chunkSize int
 }
 
-func NewReaderService(logger logger.Logger) *ResourceReader {
-	return &ResourceReader{logger: logger}
+func NewFileReaderService(ctx context.Context, logger logger.Logger, chunkSize int) *FileReaderService {
+	return &FileReaderService{ctx: ctx, logger: logger, chunkSize: chunkSize}
 }
 
-// Read will read a resource and send file as butches of bytes
-func (r *ResourceReader) Read(resource dto.Resource) chan dto.Chunk {
-	r.logger.Info(fmt.Sprintf("recourse '%v' reading started", resource.GetName()))
-
-	chunksCh := make(chan dto.Chunk, ChunksChBuffer)
-	go r.handleRead(resource, chunksCh)
-
-	return chunksCh
+func (r *FileReaderService) ReadAll(file *os.File) *model.Chunk {
+	return model.NewChunk(0)
 }
 
-func (r *ResourceReader) handleRead(resource dto.Resource, chunksCh chan dto.Chunk) {
-	defer close(chunksCh)
+// ReadByChunks - reads a file by separated chunks
+// and passed it into the channel (chunk size is setting up through env. configuration).
+func (r *FileReaderService) ReadByChunks(file *os.File, offset int64) chan *model.Chunk {
+	r.logger.Info(fmt.Sprintf("file '%v' reading started", file.Name()))
 
-	file, ferr := os.Open(resource.GetFilepath())
-	if ferr != nil {
-		r.logger.Error(ferr)
-		return
+	stat, err := file.Stat()
+	if err != nil {
+		r.logger.Info(fmt.Sprintf("file '%v' reading file stat with errors: %v", file.Name(), err))
+		return nil
 	}
-	defer func() { _ = file.Close() }()
 
-	for {
-		chunk := dto.NewChunk(ChunkSize)
+	ch := make(chan *model.Chunk, chunksChBuffer)
+	go func(offset int64, size int64) {
+		defer close(ch)
+		for {
+			select {
+			case <-r.ctx.Done():
+				r.logger.Info(fmt.Sprintf("file '%v' reading interrupted", file.Name()))
+				return
+			default:
+				// compute the current chunk buffer
+				currentChunkSize := r.chunkSize
+				if int64(currentChunkSize) > (size - offset) {
+					currentChunkSize = int(size - offset)
+					if currentChunkSize == 0 {
+						r.logger.Info(fmt.Sprintf("file '%v' reading finished properly", file.Name()))
+						return
+					}
+				}
 
-		length, err := file.Read(chunk.Data)
-		if err != nil {
-			if err == io.EOF {
-				break
+				// make a new chunk with appropriate buffer
+				chunk := model.NewChunk(currentChunkSize)
+
+				// read the current batch of bites
+				chunk.Len, err = file.ReadAt(chunk.Data, offset)
+				if err != nil {
+					r.logger.Error(err)
+					r.logger.Info(fmt.Sprintf("file '%v' reading finished with errors", file.Name()))
+					return
+				}
+				offset += int64(chunk.Len)
+
+				// cut the last chunk to its real length
+				if chunk.GetLen() < r.chunkSize {
+					chunk.SetData(chunk.GetData()[:chunk.GetLen()])
+				}
+
+				// sent the chunk to consumer
+				ch <- chunk
 			}
-			r.logger.Error(err)
-			break
 		}
-		chunk.SetLen(length)
-
-		if chunk.GetLen() < ChunkSize {
-			chunk.SetData(chunk.GetData()[:chunk.GetLen()])
-		}
-
-		if chunk.GetLen() > 0 {
-			chunksCh <- chunk
-		}
-	}
-
-	r.logger.Info(fmt.Sprintf("recourse '%v' reading finished", resource.GetName()))
+	}(offset, stat.Size())
+	return ch
 }
