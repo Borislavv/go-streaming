9fdf7707c5406b811cc618cf066eb404cd4b106d
Boris <Glazunov2142@icloud.com>
2023-09-28 17:43:30 +0300

    feat: changes in the action listener.

--- internal/infrastructure/service/streamer/action/listener/listener.go
+++ internal/infrastructure/service/streamer/action/listener/listener.go
@@ -52,9 +52,14 @@ func (l *WebSocketActionsListener) Listen(wg *sync.WaitGroup, conn *websocket.Co
 				return
 			}
 			if t == websocket.TextMessage {
-				do, data := l.communicator.Parse(b)
+				do, data, perr := l.communicator.Parse(b)
+				if perr != nil {
+					l.logger.Error(fmt.Sprintf("[%v]: %v", conn.RemoteAddr(), err.Error()))
+					return
+				}
 				if _, isSupported := supportedActionsMap[do]; isSupported {
 					actionsCh <- model.Action{Do: do, Data: data, Conn: conn}
+					l.logger.Info(fmt.Sprintf("action '%v' with data '%v' received", do, data))
 				} else {
 					l.logger.Critical(fmt.Sprintf("do: %+v, data: %+v received unsupport action", do, data))
 				}
