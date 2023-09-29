50f19ee81d940e97a6c21a3d27160db25b6146ba
Boris <Glazunov2142@icloud.com>
2023-09-29 19:31:47 +0300

    chore: updated reader chunk.

--- internal/infrastructure/service/reader/model/chunk.go
+++ internal/infrastructure/service/reader/model/chunk.go
@@ -1,35 +1,36 @@
 package model
 
+import "io"
+
 type Chunk struct {
-	Len  int64
 	Data []byte
 	Err  error
 }
 
-func NewChunk(size int64) *Chunk {
-	return &Chunk{Data: make([]byte, size)}
+func NewChunk(length int64, capacity int64) *Chunk {
+	return &Chunk{Data: make([]byte, length, capacity)}
 }
 
-func (c *Chunk) GetLen() int64 {
-	return c.Len
+func (c *Chunk) Read(p []byte) (n int, err error) {
+	if c.Data == nil || len(c.Data) == 0 {
+		return 0, io.EOF
+	}
+	n = copy(p, c.Data)
+	c.Data = c.Data[n:]
+	return n, nil
 }
-
-func (c *Chunk) SetLen(len int64) {
-	c.Len = len
+func (c *Chunk) GetLen() int {
+	return len(c.Data)
 }
-
 func (c *Chunk) GetData() []byte {
 	return c.Data
 }
-
 func (c *Chunk) SetData(data []byte) {
 	c.Data = data
 }
-
 func (c *Chunk) GetError() error {
 	return c.Err
 }
-
 func (c *Chunk) SetError(err error) {
 	c.Err = err
 }
