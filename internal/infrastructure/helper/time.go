3c3ab078fe768291846cfe6ca828c613e538417b
Boris <Glazunov2142@icloud.com>
2023-09-30 17:08:48 +0300

    chore: annotated helper.ParseTime.

--- internal/infrastructure/helper/time.go
+++ internal/infrastructure/helper/time.go
@@ -11,6 +11,10 @@ var dateLayouts = []string{
 	"2006-01-02",
 }
 
+// ParseTime is a user helper function which parsing three types of datetime strings:
+// 	1. 2006-01-02T15:04:05Z07:00
+//	2. 2006-01-02T15:04:05
+// 	3. 2006-01-02
 func ParseTime(date string) (time.Time, error) {
 	for _, layout := range dateLayouts {
 		parsed, err := time.Parse(layout, date)
