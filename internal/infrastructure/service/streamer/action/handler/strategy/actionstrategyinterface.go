cd514043c78f4bd8229aab88f48457e5da56183c
Boris <Glazunov2142@icloud.com>
2023-09-30 16:58:45 +0300

    chore: annotated ActionStrategy interface.

--- internal/infrastructure/service/streamer/action/handler/strategy/actionstrategyinterface.go
+++ internal/infrastructure/service/streamer/action/handler/strategy/actionstrategyinterface.go
@@ -5,6 +5,8 @@ import (
 )
 
 type ActionStrategy interface {
+	// IsAppropriate - method will tell the service architect that the strategy is acceptable.
 	IsAppropriate(action model.Action) bool
+	// Do is a method which contains the useful work of target strategy.
 	Do(action model.Action) error
 }
