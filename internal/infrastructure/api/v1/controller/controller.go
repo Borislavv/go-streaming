9c42573364589d82489741d304c1cda17c47d8d4
Boris <Glazunov2142@icloud.com>
2023-09-30 17:04:52 +0300

    chore: annotated AddRoute into the controller interface.

--- internal/infrastructure/api/v1/controller/controller.go
+++ internal/infrastructure/api/v1/controller/controller.go
@@ -5,5 +5,13 @@ import (
 )
 
 type Controller interface {
+	// AddRoute is a method which must take a *mux.Router and add new route.
+	// Must be implemented into each RestApi and so on controllers.
+	//
+	// Commonly may be represented as:
+	// 	router.
+	//		Path(CreatePath).
+	//		HandlerFunc(c.Create).
+	//		Methods(http.MethodPost)
 	AddRoute(router *mux.Router)
 }
