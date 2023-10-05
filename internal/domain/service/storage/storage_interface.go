e82b71c962200b40bf9c3877ef5b89455b27cece
Boris <Glazunov2142@icloud.com>
2023-10-05 16:14:11 +0300

    chore: added todo comment.

--- internal/domain/service/storage/storage_interface.go
+++ internal/domain/service/storage/storage_interface.go
@@ -7,6 +7,10 @@ import (
 type Storage interface {
 	// Has is checking whether the file already exists.
 	Has(filename string) (has bool, err error)
+
+	// TODO This both methods must be merged
 	// Store is saving file and calculating new hashed name.
 	Store(name string, part *multipart.Part) (length int64, filename string, filepath string, err error)
+	// StoreFile is saving file and calculating new hashed name.
+	StoreFile(name string, part *multipart.File) (length int64, filename string, filepath string, err error)
 }
