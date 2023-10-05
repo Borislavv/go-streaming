a586d8021fa3b751b4ce7e8b04d058cfda846042
Boris <Glazunov2142@icloud.com>
2023-10-05 16:17:37 +0300

    feat: resolved storage interface.

--- internal/domain/service/storage/storage_interface.go
+++ internal/domain/service/storage/storage_interface.go
@@ -1,16 +1,12 @@
 package storage
 
 import (
-	"mime/multipart"
+	"io"
 )
 
 type Storage interface {
 	// Has is checking whether the file already exists.
 	Has(filename string) (has bool, err error)
-
-	// TODO This both methods must be merged
 	// Store is saving file and calculating new hashed name.
-	Store(name string, part *multipart.Part) (length int64, filename string, filepath string, err error)
-	// StoreFile is saving file and calculating new hashed name.
-	StoreFile(name string, part *multipart.File) (length int64, filename string, filepath string, err error)
+	Store(name string, reader io.Reader) (length int64, filename string, filepath string, err error)
 }
