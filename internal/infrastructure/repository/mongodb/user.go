c5167d3e5d343a3de81b58b4527b778209c3e801
Boris <Glazunov2142@icloud.com>
2023-10-10 12:53:49 +0300

    feat: implemented Find method of user repository.

--- internal/infrastructure/repository/mongodb/user.go
+++ internal/infrastructure/repository/mongodb/user.go
@@ -1,6 +1,16 @@
 package mongodb
 
-import "github.com/Borislavv/video-streaming/internal/domain/errors"
+import (
+	"context"
+	"github.com/Borislavv/video-streaming/internal/domain/agg"
+	"github.com/Borislavv/video-streaming/internal/domain/errors"
+	"github.com/Borislavv/video-streaming/internal/domain/logger"
+	"github.com/Borislavv/video-streaming/internal/domain/vo"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"sync"
+	"time"
+)
 
 const UserCollection = "users"
 
@@ -10,3 +20,34 @@ var (
 	UserInsertingFailedError = errors.NewInternalValidationError("unable to store 'user' or get inserted 'id'")
 	UserWasNotDeletedError   = errors.NewInternalValidationError("user was not deleted")
 )
+
+type UserRepository struct {
+	db      *mongo.Collection
+	mu      *sync.Mutex
+	logger  logger.Logger
+	timeout time.Duration
+}
+
+func NewUserRepository(db *mongo.Database, logger logger.Logger, timeout time.Duration) *UserRepository {
+	return &UserRepository{
+		db:      db.Collection(UserCollection),
+		logger:  logger,
+		mu:      &sync.Mutex{},
+		timeout: timeout,
+	}
+}
+
+func (r *UserRepository) Find(ctx context.Context, id vo.ID) (user *agg.User, err error) {
+	qCtx, cancel := context.WithTimeout(ctx, r.timeout)
+	defer cancel()
+
+	user = &agg.User{}
+	if err = r.db.FindOne(qCtx, bson.M{"_id": bson.M{"$eq": id.Value}}).Decode(user); err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, r.logger.InfoPropagate(UserNotFoundByIdError)
+		}
+		return nil, r.logger.ErrorPropagate(err)
+	}
+
+	return user, nil
+}
