da6ea5e693c54286962d7cc0145586b25bdb62f3
Boris <Glazunov2142@icloud.com>
2023-10-10 12:31:33 +0300

    feat: new user CRUD service with them interface.

---
+++ internal/domain/service/user/crud_interface.go
@@ -0,0 +1,13 @@
+package user
+
+import (
+	"github.com/Borislavv/video-streaming/internal/domain/agg"
+	"github.com/Borislavv/video-streaming/internal/domain/dto"
+)
+
+type CRUD interface {
+	Get(reqDTO dto.GetRequest) (*agg.User, error)
+	Create(reqDTO dto.CreateRequest) (*agg.User, error)
+	Update(reqDTO dto.UpdateRequest) (*agg.User, error)
+	Delete(reqDTO dto.DeleteRequest) error
+}
