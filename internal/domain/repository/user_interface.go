9729235534632b00ea58a0765e9e057df3792265
Boris <Glazunov2142@icloud.com>
2023-10-10 12:14:48 +0300

    feat: added user repository interface.

---
+++ internal/domain/repository/user_interface.go
@@ -0,0 +1,14 @@
+package repository
+
+import (
+	"context"
+	"github.com/Borislavv/video-streaming/internal/domain/agg"
+	"github.com/Borislavv/video-streaming/internal/domain/vo"
+)
+
+type User interface {
+	Find(ctx context.Context, id vo.ID) (*agg.User, error)
+	Insert(ctx context.Context, user *agg.User) (*agg.User, error)
+	Update(ctx context.Context, user *agg.User) (*agg.User, error)
+	Remove(ctx context.Context, user *agg.User) error
+}
