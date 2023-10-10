0c2d27827f81746f0fbe68ed79677d61a82b099c
Boris <Glazunov2142@icloud.com>
2023-10-10 12:14:27 +0300

    feat: improved user entity.

--- internal/domain/entity/user.go
+++ internal/domain/entity/user.go
@@ -1,6 +1,9 @@
 package entity
 
+import "github.com/Borislavv/video-streaming/internal/domain/vo"
+
 type User struct {
+	ID       vo.ID  `json:"ID" bson:",inline"`
 	Username string `json:"username" bson:"username"`
 	Password string `json:"password" bson:"password"`
 	Birthday string `json:"birthday" bson:"birthday,omitempty"`
