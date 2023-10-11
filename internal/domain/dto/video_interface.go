fc783ead8b21477a1e58c5900eb8e68e0a99f116
Boris <Glazunov2142@icloud.com>
2023-10-11 13:20:25 +0300

    feat: highlighted video interface.

---
+++ internal/domain/dto/video_interface.go
@@ -0,0 +1,32 @@
+package dto
+
+import (
+	"github.com/Borislavv/video-streaming/internal/domain/vo"
+	"time"
+)
+
+type CreateVideoRequest interface {
+	GetName() string
+	GetResourceID() vo.ID
+	GetDescription() string
+}
+
+type UpdateVideoRequest interface {
+	GetId() vo.ID
+	GetName() string
+	GetResourceID() vo.ID
+	GetDescription() string
+}
+
+type GetVideoRequest interface {
+	GetId() vo.ID
+}
+type ListVideoRequest interface {
+	GetName() string         // path of name
+	GetCreatedAt() time.Time // concrete search date point
+	GetFrom() time.Time      // search date limit from
+	GetTo() time.Time        // search date limit to
+	PaginatedRequest
+}
+
+type DeleteVideoRequest GetVideoRequest
