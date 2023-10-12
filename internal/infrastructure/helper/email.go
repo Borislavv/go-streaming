32db2c54bc2a15d3fcf9be1e4363baec6eedf2b5
Boris <Glazunov2142@icloud.com>
2023-10-12 16:21:42 +0300

    feat: new email helper.

---
+++ internal/infrastructure/helper/email.go
@@ -0,0 +1,9 @@
+package helper
+
+import "regexp"
+
+func IsValidEmail(email string) bool {
+	regex := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+	match, _ := regexp.MatchString(regex, email)
+	return match
+}
