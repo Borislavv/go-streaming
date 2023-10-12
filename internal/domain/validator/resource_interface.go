818059955bc320dcf79643747c01e58c391bd95b
Boris <Glazunov2142@icloud.com>
2023-10-12 16:20:28 +0300

    feat: new resourecvalidator interface.

---
+++ internal/domain/validator/resource_interface.go
@@ -0,0 +1,13 @@
+package validator
+
+import (
+	"github.com/Borislavv/video-streaming/internal/domain/agg"
+	"github.com/Borislavv/video-streaming/internal/domain/dto"
+	"github.com/Borislavv/video-streaming/internal/domain/entity"
+)
+
+type Resource interface {
+	ValidateUploadRequestDTO(req dto.UploadResourceRequest) error
+	ValidateEntity(entity entity.Resource) error
+	ValidateAggregate(agg *agg.Resource) error
+}
