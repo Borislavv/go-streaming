66fbcdda839e4a7cd67c433d3bee6c1112830ad3
Boris <Glazunov2142@icloud.com>
2023-10-12 16:32:46 +0300

    feat: resolved user create controller.

--- internal/infrastructure/api/v1/controller/rest/user/create.go
+++ internal/infrastructure/api/v1/controller/rest/user/create.go
@@ -1,8 +1,11 @@
 package user
 
 import (
+	"github.com/Borislavv/video-streaming/internal/domain/builder"
+	"github.com/Borislavv/video-streaming/internal/domain/logger"
+	"github.com/Borislavv/video-streaming/internal/domain/service/video"
+	"github.com/Borislavv/video-streaming/internal/infrastructure/api/v1/response"
 	"github.com/gorilla/mux"
-	"log"
 	"net/http"
 )
 
@@ -10,16 +13,41 @@ const CreatePath = "/user"
 
 // CreateUserController - not implemented yet.
 type CreateUserController struct {
+	logger   logger.Logger
+	builder  builder.Video
+	service  video.CRUD
+	response response.Responder
 }
 
-func NewCreateController() *CreateUserController {
-	return &CreateUserController{}
+func NewCreateController(
+	logger logger.Logger,
+	builder builder.Video,
+	service video.CRUD,
+	response response.Responder,
+) *CreateUserController {
+	return &CreateUserController{
+		logger:   logger,
+		builder:  builder,
+		service:  service,
+		response: response,
+	}
 }
 
 func (c *CreateUserController) Create(w http.ResponseWriter, r *http.Request) {
-	if _, err := w.Write([]byte("Sorry, the route is not implemented yet :(")); err != nil {
-		log.Fatalln(err)
+	userDTO, err := c.builder.BuildCreateRequestDTOFromRequest(r)
+	if err != nil {
+		c.response.Respond(w, c.logger.LogPropagate(err))
+		return
 	}
+
+	userAgg, err := c.service.Create(userDTO)
+	if err != nil {
+		c.response.Respond(w, c.logger.LogPropagate(err))
+		return
+	}
+
+	c.response.Respond(w, userAgg)
+	w.WriteHeader(http.StatusCreated)
 }
 
 func (c *CreateUserController) AddRoute(router *mux.Router) {
