92aa042a7d71ed6838890df04826e86eebc6e051
Boris <Glazunov2142@icloud.com>
2023-10-15 13:45:10 +0300

    feat: new method of storage (filesystem).

--- internal/domain/service/storage/storage_interface.go
+++ internal/domain/service/storage/storage_interface.go
@@ -9,4 +9,6 @@ type Storage interface {
 	Has(filename string) (has bool, err error)
 	// Store is saving file and calculating new hashed name.
 	Store(name string, reader io.Reader) (length int64, filename string, filepath string, err error)
+	// Remove is delete the file by name from resources directory.
+	Remove(name string) (err error)
 }
