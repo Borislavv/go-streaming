70242d16e9ea8b10330d52864c8640c934aa453e
Boris <Glazunov2142@icloud.com>
2023-10-17 10:04:54 +0300

    chore: changed params. naming of request param. extractor.

--- internal/domain/service/extractor/param_extractor_interface.go
+++ internal/domain/service/extractor/param_extractor_interface.go
@@ -3,7 +3,7 @@ package extractor
 import "net/http"
 
 type RequestParams interface {
-	Parameters(req *http.Request) map[string]string
+	Parameters(r *http.Request) map[string]string
 	HasParameter(param string, req *http.Request) bool
 	GetParameter(param string, req *http.Request) (string, error)
 }
