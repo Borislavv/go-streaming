6be8a49e298be0805a5100a6e5c07ebc2ce016ad
Boris <Glazunov2142@icloud.com>
2023-10-17 09:55:12 +0300

    feat: added two methods into the user interface.

--- internal/domain/builder/user_interface.go
+++ internal/domain/builder/user_interface.go
@@ -10,5 +10,7 @@ type User interface {
 	BuildGetRequestDTOFromRequest(r *http.Request) (*dto.UserGetRequestDTO, error)
 	BuildCreateRequestDTOFromRequest(r *http.Request) (*dto.UserCreateRequestDTO, error)
 	BuildAggFromCreateRequestDTO(dto dto.CreateUserRequest) (*agg.User, error)
+	BuildUpdateRequestDTOFromRequest(r *http.Request) (*dto.UserUpdateRequestDTO, error)
+	BuildAggFromUpdateRequestDTO(dto dto.UpdateUserRequest) (*agg.User, error)
 	BuildDeleteRequestDTOFromRequest(r *http.Request) (*dto.UserDeleteRequestDto, error)
 }
