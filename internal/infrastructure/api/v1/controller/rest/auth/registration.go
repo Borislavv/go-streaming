a447ee8fff4b2e7060201e8da49615ef789797fc
Boris <Glazunov2142@icloud.com>
2023-10-18 13:11:37 +0300

    feat: implemented registration controller. #duplicated
    Duplicated the user create controller. Probably, the user create controller must be removed.

--- internal/infrastructure/api/v1/controller/rest/auth/registration.go
+++ internal/infrastructure/api/v1/controller/rest/auth/registration.go
@@ -1,25 +1,52 @@
 package auth
 
 import (
+	"github.com/Borislavv/video-streaming/internal/domain/builder"
+	"github.com/Borislavv/video-streaming/internal/domain/logger"
+	"github.com/Borislavv/video-streaming/internal/domain/service/user"
+	"github.com/Borislavv/video-streaming/internal/infrastructure/api/v1/response"
 	"github.com/gorilla/mux"
-	"log"
 	"net/http"
 )
 
 const RegistrationPath = "/registration"
 
-// RegistrationController - not implemented yet.
 type RegistrationController struct {
+	logger   logger.Logger
+	builder  builder.User
+	service  user.CRUD
+	response response.Responder
 }
 
-func NewRegistrationController() *RegistrationController {
-	return &RegistrationController{}
+func NewRegistrationController(
+	logger logger.Logger,
+	builder builder.User,
+	service user.CRUD,
+	response response.Responder,
+) *RegistrationController {
+	return &RegistrationController{
+		logger:   logger,
+		builder:  builder,
+		service:  service,
+		response: response,
+	}
 }
 
 func (c *RegistrationController) Registration(w http.ResponseWriter, r *http.Request) {
-	if _, err := w.Write([]byte("Sorry, the route is not implemented yet :(")); err != nil {
-		log.Fatalln(err)
+	userDTO, err := c.builder.BuildCreateRequestDTOFromRequest(r)
+	if err != nil {
+		c.response.Respond(w, c.logger.LogPropagate(err))
+		return
 	}
+
+	userAgg, err := c.service.Create(userDTO)
+	if err != nil {
+		c.response.Respond(w, c.logger.LogPropagate(err))
+		return
+	}
+
+	c.response.Respond(w, userAgg)
+	w.WriteHeader(http.StatusCreated)
 }
 
 func (c *RegistrationController) AddRoute(router *mux.Router) {
