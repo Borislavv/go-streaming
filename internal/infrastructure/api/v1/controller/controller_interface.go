c6173211300c1987589dce859ecacf0566949e64
Boris <Glazunov2142@icloud.com>
2023-10-18 13:18:57 +0300

    feat: improved doc. comment for AddRoute method of Controller interface.

--- internal/infrastructure/api/v1/controller/controller_interface.go
+++ internal/infrastructure/api/v1/controller/controller_interface.go
@@ -6,7 +6,8 @@ import (
 
 type Controller interface {
 	// AddRoute is a method which must take a *mux.Router and add new route.
-	// Must be implemented into each RestApi and so on controllers.
+	// Must be implemented into each RestApi and so on controllers for inject them
+	// into the server for serving them.
 	//
 	// Commonly may be represented as:
 	// 	router.
