ff430126e107d29d58a49eacf2c47e9129860152
Boris <Glazunov2142@icloud.com>
2023-10-20 15:56:30 +0300

    feat: new token service.

---
+++ internal/domain/service/auth/token_interface.go
@@ -0,0 +1,16 @@
+package auth
+
+import (
+	"github.com/Borislavv/video-streaming/internal/domain/agg"
+	"net/http"
+)
+
+type Tokenizer interface {
+	New(user *agg.User) (token string, err error)
+	Has(r *http.Request) bool
+	Get(r *http.Request) (token string, err error)
+	Set(w http.ResponseWriter, r *http.Request, user *agg.User) error
+	Refresh(w http.ResponseWriter, user *agg.User) error
+	IsValid(w http.ResponseWriter, r *http.Request, user *agg.User) (ok bool, err error)
+	Remove(w http.ResponseWriter)
+}
