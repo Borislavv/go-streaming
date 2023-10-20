3c0e3cfd228b74c5a7217755c3110d8164d3026f
Boris <Glazunov2142@icloud.com>
2023-10-20 15:54:11 +0300

    feat: implemented auth. builder.

---
+++ internal/domain/builder/auth_interface.go
@@ -0,0 +1,10 @@
+package builder
+
+import (
+	"github.com/Borislavv/video-streaming/internal/domain/dto"
+	"net/http"
+)
+
+type Auth interface {
+	BuildAuthRequestDTOFromRequest(r *http.Request) (dto.AuthRequest, error)
+}
