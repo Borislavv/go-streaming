872cdcf16e28d01941a7e89fb4c46aeea71b2e42
Boris <Glazunov2142@icloud.com>
2023-10-26 12:00:00 +0300

    chore: expanded user aggregate by audio IDs.

--- internal/domain/agg/user.go
+++ internal/domain/agg/user.go
@@ -8,6 +8,8 @@ import (
 type User struct {
 	entity.User `bson:",inline"`
 
-	VideoIDs  []vo.ID      `json:"videos" bson:"videos"`
+	VideoIDs []vo.ID `json:"videos" bson:"videos"`
+	AudioIDs []vo.ID `json:"audios" bson:"audios"`
+
 	Timestamp vo.Timestamp `json:"timestamp" bson:",inline"`
 }
