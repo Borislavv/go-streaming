029dc673047423f59a7a9f43acd228b72c9e78c2
Boris <Glazunov2142@icloud.com>
2023-10-27 16:30:41 +0300

    feat: new entity named blocked_token.

---
+++ internal/domain/entity/blocked_token.go
@@ -0,0 +1,16 @@
+package entity
+
+import (
+	"github.com/Borislavv/video-streaming/internal/domain/vo"
+	"time"
+)
+
+type BlockedToken struct {
+	ID        vo.ID     `json:"id,omitempty" bson:"_id,omitempty,inline"`
+	Token     string    `bson:"value"`
+	BlockedAt time.Time `bson:"blockedAt"`
+}
+
+func (r BlockedToken) GetID() vo.ID {
+	return r.ID
+}
