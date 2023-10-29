adb74a9e229173168538809bc5892fa79c08bc6b
Boris <Glazunov2142@icloud.com>
2023-10-29 13:12:04 +0300

    chore: resolve map storage (intermediate commit).

--- internal/infrastructure/service/cacher/storage_map.go
+++ internal/infrastructure/service/cacher/storage_map.go
@@ -3,6 +3,7 @@ package cacher
 import (
 	"context"
 	"sync"
+	"time"
 )
 
 type MapCacheStorage struct {
@@ -24,6 +25,60 @@ func NewMapCacheStorage(ctx context.Context) *MapCacheStorage {
 	}
 }
 
-func (c *MapCacheStorage) Get(key string, callable func() (data interface{}, err error)) {
+func (c *MapCacheStorage) Get(key string, fn func(CacheItem) (data interface{}, err error)) (data interface{}, err error) {
+	item, found := c.get(key)
+	if found {
+		return item.data, nil
+	}
 
+	item, err = c.compute(fn)
+	if err != nil {
+		return nil, err
+	}
+
+	return c.set(key, item), nil
+}
+
+func (c *MapCacheStorage) get(key string) (item *Item, found bool) {
+	defer c.mu.RUnlock()
+	c.mu.RLock()
+	cacheItem, found := c.storage[key]
+	if found {
+		foundItem, ok := cacheItem.(*Item)
+		if ok {
+			return foundItem, true
+		}
+	}
+	return nil, false
+}
+
+func (c *MapCacheStorage) compute(fn func(CacheItem) (data interface{}, err error)) (item *Item, err error) {
+	item = NewCacheItem()
+	data, err := fn(item)
+	if err != nil {
+		return nil, err
+	}
+	item.data = data
+	item.addedAt = time.Now()
+	return item, nil
+}
+
+func (c *MapCacheStorage) set(key string, item *Item) (data interface{}) {
+	defer c.mu.Unlock()
+	c.mu.Lock()
+	cacheItem, found := c.storage[key]
+	if found {
+		foundItem, ok := cacheItem.(*Item)
+		if ok {
+			return foundItem.data
+		}
+	}
+	c.storage[key] = item
+	return item.data
+}
+
+func (c *MapCacheStorage) Del(key string) {
+	defer c.mu.Unlock()
+	c.mu.Lock()
+	delete(c.storage, key)
 }
