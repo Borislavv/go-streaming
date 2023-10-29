43041e61228e0494f85564029db1cca6d0edb7e7
Boris <Glazunov2142@icloud.com>
2023-10-29 13:11:13 +0300

    chore: removed cache service (intermediate commit).

--- internal/infrastructure/service/cacher/cache.go
+++ internal/infrastructure/service/cacher/cache.go
@@ -1,24 +1,4 @@
 package cacher
 
-import "time"
-
-type CacheItem struct {
-	data      interface{}
-	addedAt   time.Time
-	expiresAt time.Time
-}
-
-func NewCacheItem() *CacheItem {
-	return &CacheItem{
-		data:      struct{}{},
-		addedAt:   time.Now(),
-		expiresAt: time.Time{},
-	}
-}
-
-func (i *CacheItem) SetTTL(ttl time.Duration) {
-	i.expiresAt = time.Now().Add(ttl)
-}
-
 type Cache struct {
 }
