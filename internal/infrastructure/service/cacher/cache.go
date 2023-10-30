9b624bc16b8848e918ff1895adacd65916b595d7
Boris <Glazunov2142@icloud.com>
2023-10-30 11:28:33 +0300

    feat: implemented cache_interface.

--- internal/infrastructure/service/cacher/cache.go
+++ internal/infrastructure/service/cacher/cache.go
@@ -1,4 +1,27 @@
 package cacher
 
+import "runtime"
+
 type Cache struct {
+	storage   Storage
+	displacer Displacer
+}
+
+func NewCache(storage Storage, displacer Displacer) *Cache {
+	c := &Cache{
+		storage:   storage,
+		displacer: displacer,
+	}
+
+	runtime.SetFinalizer(c, displacer.Stop)
+
+	return c
+}
+
+func (c *Cache) Get(key string, fn func(CacheItem) (data interface{}, err error)) (data interface{}, err error) {
+	return c.storage.Get(key, fn)
+}
+
+func (c *Cache) Delete(key string) {
+	c.storage.Delete(key)
 }
