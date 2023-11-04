8c717a71db267a69dbac5a23aeaed8469592aa17
Boris <Glazunov2142@icloud.com>
2023-11-04 12:00:00 +0300

    chore: start of using auth service for create videos.

--- internal/infrastructure/api/v1/controller/rest/video/create.go
+++ internal/infrastructure/api/v1/controller/rest/video/create.go
@@ -3,6 +3,7 @@ package video
 import (
 	"github.com/Borislavv/video-streaming/internal/domain/builder"
 	"github.com/Borislavv/video-streaming/internal/domain/logger"
+	"github.com/Borislavv/video-streaming/internal/domain/service/authenticator"
 	"github.com/Borislavv/video-streaming/internal/domain/service/video"
 	"github.com/Borislavv/video-streaming/internal/infrastructure/api/v1/response"
 	"github.com/gorilla/mux"
@@ -12,34 +13,43 @@ import (
 const CreatePath = "/video"
 
 type CreateVideoController struct {
-	logger   logger.Logger
-	builder  builder.Video
-	service  video.CRUD
-	response response.Responder
+	logger      logger.Logger
+	builder     builder.Video
+	crudService video.CRUD
+	authService authenticator.Authenticator
+	response    response.Responder
 }
 
 func NewCreateController(
 	logger logger.Logger,
 	builder builder.Video,
-	service video.CRUD,
+	crudService video.CRUD,
+	authService authenticator.Authenticator,
 	response response.Responder,
 ) *CreateVideoController {
 	return &CreateVideoController{
-		logger:   logger,
-		builder:  builder,
-		service:  service,
-		response: response,
+		logger:      logger,
+		builder:     builder,
+		crudService: crudService,
+		authService: authService,
+		response:    response,
 	}
 }
 
 func (c *CreateVideoController) Create(w http.ResponseWriter, r *http.Request) {
+	userID, err := c.authService.IsAuthed(r)
+	if err != nil {
+		c.response.Respond(w, c.logger.LogPropagate(err))
+		return
+	}
+
 	videoDTO, err := c.builder.BuildCreateRequestDTOFromRequest(r)
 	if err != nil {
 		c.response.Respond(w, c.logger.LogPropagate(err))
 		return
 	}
 
-	videoAgg, err := c.service.Create(videoDTO)
+	videoAgg, err := c.crudService.Create(videoDTO)
 	if err != nil {
 		c.response.Respond(w, c.logger.LogPropagate(err))
 		return
