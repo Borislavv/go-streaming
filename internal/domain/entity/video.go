d3d2ea05e77c7c8fee8c9c4b06c8019dcae3552e
Boris <Glazunov2142@icloud.com>
2023-11-08 12:00:00 +0300

    chore: video entity expanded by userID.

--- internal/domain/entity/video.go
+++ internal/domain/entity/video.go
@@ -4,6 +4,7 @@ import "github.com/Borislavv/video-streaming/internal/domain/vo"
 
 type Video struct {
 	ID          vo.ID  `json:"id" bson:",inline"`
+	UserID      vo.ID  `json:"userID" bson:"userID"`
 	Name        string `json:"name" bson:"name"`
 	Description string `json:"description" bson:"description,omitempty"`
 }
