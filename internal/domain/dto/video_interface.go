b96c15d21270d9b31d566400b287ef7818ea18fe
Boris <Glazunov2142@icloud.com>
2023-11-09 12:00:00 +0300

    chore: improved video dto interface.

--- internal/domain/dto/video_interface.go
+++ internal/domain/dto/video_interface.go
@@ -15,15 +15,18 @@ type CreateVideoRequest interface {
 type UpdateVideoRequest interface {
 	GetID() vo.ID
 	GetName() string
+	GetUserID() vo.ID
 	GetResourceID() vo.ID
 	GetDescription() string
 }
 
 type GetVideoRequest interface {
 	GetID() vo.ID
+	GetUserID() vo.ID
 }
 type ListVideoRequest interface {
 	GetName() string         // path of name
+	GetUserID() vo.ID        // user identifier
 	GetCreatedAt() time.Time // concrete search date point
 	GetFrom() time.Time      // search date limit from
 	GetTo() time.Time        // search date limit to
