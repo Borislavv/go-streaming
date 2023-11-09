ef09ed986593a77fa154f868d2b84ba1f5e4c515
Boris <Glazunov2142@icloud.com>
2023-11-09 12:00:00 +0300

    chore: added access check into the video create/update methods.

--- internal/domain/service/video/crud.go
+++ internal/domain/service/video/crud.go
@@ -7,6 +7,7 @@ import (
 	"github.com/Borislavv/video-streaming/internal/domain/dto"
 	"github.com/Borislavv/video-streaming/internal/domain/logger"
 	"github.com/Borislavv/video-streaming/internal/domain/repository"
+	"github.com/Borislavv/video-streaming/internal/domain/service/accessor"
 	"github.com/Borislavv/video-streaming/internal/domain/service/resource"
 	"github.com/Borislavv/video-streaming/internal/domain/validator"
 )
@@ -16,6 +17,7 @@ type CRUDService struct {
 	logger          logger.Logger
 	builder         builder.Video
 	validator       validator.Video
+	accessor        accessor.Accessor
 	repository      repository.Video
 	resourceService resource.CRUD
 }
@@ -25,6 +27,7 @@ func NewCRUDService(
 	logger logger.Logger,
 	builder builder.Video,
 	validator validator.Video,
+	accessor accessor.Accessor,
 	repository repository.Video,
 	resourceService resource.CRUD,
 ) *CRUDService {
@@ -33,17 +36,21 @@ func NewCRUDService(
 		logger:          logger,
 		builder:         builder,
 		validator:       validator,
+		accessor:        accessor,
 		repository:      repository,
 		resourceService: resourceService,
 	}
 }
 
+// Get - will fetch a single video aggregate by ID and specified user.
+// Access check to video is unnecessary because the query will fetch video only for specified user.
 func (s *CRUDService) Get(reqDTO dto.GetVideoRequest) (*agg.Video, error) {
 	// validation of input request
 	if err := s.validator.ValidateGetRequestDTO(reqDTO); err != nil {
 		return nil, s.logger.LogPropagate(err)
 	}
 
+	// fetching a video by id and user
 	video, err := s.repository.Find(s.ctx, reqDTO.GetID())
 	if err != nil {
 		return nil, s.logger.LogPropagate(err)
@@ -52,12 +59,15 @@ func (s *CRUDService) Get(reqDTO dto.GetVideoRequest) (*agg.Video, error) {
 	return video, nil
 }
 
+// List - will fetch a video list of aggregates by given request and specified user.
+// Access check to video is unnecessary because the query will fetch a video list only for specified user.
 func (s *CRUDService) List(reqDTO dto.ListVideoRequest) (list []*agg.Video, total int64, err error) {
 	// validation of input request
 	if err = s.validator.ValidateListRequestDTO(reqDTO); err != nil {
 		return nil, 0, s.logger.LogPropagate(err)
 	}
 
+	// fetching a video list by request params. and user
 	list, total, err = s.repository.FindList(s.ctx, reqDTO)
 	if err != nil {
 		return nil, 0, s.logger.LogPropagate(err)
@@ -80,6 +90,11 @@ func (s *CRUDService) Create(reqDTO dto.CreateVideoRequest) (*agg.Video, error) 
 
 	// validation of an aggregate
 	if err = s.validator.ValidateAggregate(videoAgg); err != nil {
+		return nil, s.logger.LogPropagate(err)
+	}
+
+	// check that all aggregate's entities belong to user
+	if err = s.accessor.IsGranted(reqDTO.GetUserID(), videoAgg); err != nil {
 		return nil, s.logger.LogPropagate(err)
 	}
 
@@ -106,6 +121,11 @@ func (s *CRUDService) Update(reqDTO dto.UpdateVideoRequest) (*agg.Video, error) 
 
 	// validation of an aggregate
 	if err = s.validator.ValidateAggregate(videoAgg); err != nil {
+		return nil, s.logger.LogPropagate(err)
+	}
+
+	// check that all aggregate's entities belong to user
+	if err = s.accessor.IsGranted(reqDTO.GetUserID(), videoAgg); err != nil {
 		return nil, s.logger.LogPropagate(err)
 	}
 
