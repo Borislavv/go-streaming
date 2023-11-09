4495472016ae79ae194f8b923b6e6b0348c81733
Boris <Glazunov2142@icloud.com>
2023-11-09 12:00:00 +0300

    chore: improved user aggregate.

--- internal/domain/agg/user.go
+++ internal/domain/agg/user.go
@@ -8,9 +8,5 @@ import (
 type User struct {
 	entity.User `bson:",inline"`
 
-	VideoIDs    []vo.ID `json:"videos" bson:"videos"`
-	AudioIDs    []vo.ID `json:"audios" bson:"audios"`
-	ResourceIDs []vo.ID `json:"resources" bson:"resources"`
-
 	Timestamp vo.Timestamp `json:"timestamp" bson:",inline"`
 }
