20683b0506ef1f75b3a0d91ab40b2af2b2bb30d4
Boris <Glazunov2142@icloud.com>
2023-11-09 12:00:00 +0300

    chore: expanded audio entity.

--- internal/domain/entity/audio.go
+++ internal/domain/entity/audio.go
@@ -4,6 +4,7 @@ import "github.com/Borislavv/video-streaming/internal/domain/vo"
 
 type Audio struct {
 	ID          vo.ID  `json:"id,omitempty" bson:"_id,omitempty,inline"`
+	UserID      vo.ID  `json:"userID" bson:"userID"`
 	Name        string `bson:"name"`
 	Description string `bson:"description,omitempty"`
 }
