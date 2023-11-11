3fe8394c534eae6b966fa2a8eb13126632f80dc0
Boris <Glazunov2142@icloud.com>
2023-11-11 14:26:21 +0300

    chore: moved user and video interfaces.

--- internal/domain/repository/storage/user_interface.go
+++ internal/domain/repository/storage/user_interface.go
@@ -7,8 +7,8 @@ import (
 )
 
 type User interface {
-	Find(ctx context.Context, id vo.ID) (*agg.User, error)
-	FindByEmail(ctx context.Context, email string) (*agg.User, error)
+	FindOneByID(ctx context.Context, id vo.ID) (*agg.User, error)
+	FindOneByEmail(ctx context.Context, email string) (*agg.User, error)
 	Insert(ctx context.Context, user *agg.User) (*agg.User, error)
 	Update(ctx context.Context, user *agg.User) (*agg.User, error)
 	Remove(ctx context.Context, user *agg.User) error
