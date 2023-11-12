90198a21235b7fdaa8022aaf92f6f447892fc2fe
Boris <Glazunov2142@icloud.com>
2023-11-12 11:40:15 +0300

    feat: blocked_token constructor.

--- internal/domain/agg/blocked_token.go
+++ internal/domain/agg/blocked_token.go
@@ -3,10 +3,25 @@ package agg
 import (
 	"github.com/Borislavv/video-streaming/internal/domain/entity"
 	"github.com/Borislavv/video-streaming/internal/domain/vo"
+	"time"
 )
 
 type BlockedToken struct {
 	entity.BlockedToken `bson:",inline"`
 
 	Timestamp vo.Timestamp `json:"timestamp" bson:",inline"`
+}
+
+func NewBlockedToken(token string, userID vo.ID) *BlockedToken {
+	return &BlockedToken{
+		BlockedToken: entity.BlockedToken{
+			Value:     token,
+			UserID:    userID,
+			BlockedAt: time.Now(),
+		},
+		Timestamp: vo.Timestamp{
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Time{},
+		},
+	}
 }
