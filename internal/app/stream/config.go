e7307d4e66df7459bee9364d25fc16ba98edfc18
Boris <Glazunov2142@icloud.com>
2023-11-12 13:36:33 +0300

    feat: expanded streaming config.

--- internal/app/stream/config.go
+++ internal/app/stream/config.go
@@ -20,4 +20,22 @@ type config struct {
 	// ChunkSize is a value which means the size of one chunk while reading the file when streaming a resource.
 	// By default, it's 1mb.
 	ChunkSize int `env:"FILE_READER_CHUNK_SIZE" envDefault:"1048576"`
+	// >>> APPLICATION <<<
+	// JwtSecretSalt is a secret string which further will convert to slice of bytes and will be provided
+	// as a salt for signature the jwt tokens.
+	// If this variable an empty or omitted then will be generated random salt which is alive while application instance
+	// is running (note: you cannot get access to this value, and it will not be provided anywhere as an output. If
+	// you need access to this value, and you need to share this value, then sat up your own secret string).
+	JwtSecretSalt string `env:"JWT_SECRET_SALT" envDefault:""`
+	// JwtTokenIssuer is an issuer of JWT token. This variable helps determine which service issued the token
+	// (commonly used for verify that token was created by service of your system, for example,
+	// if you have more than one service which able for issue a token).
+	JwtTokenIssuer string `env:"JWT_TOKEN_ISSUER" envDefault:"streaming_service"`
+	// JwtTokenAcceptedIssuers is a string with another JwtTokenIssuer values separated by delimiter. This values will
+	// be accepted while token payload verification.
+	JwtTokenAcceptedIssuers string `env:"JWT_TOKEN_ACCEPTED_ISSUERS" envDefault:"auth_service,streaming_service"`
+	// JwtTokenExpiresAfter is a value which defined TTL of token in seconds. Default: `86400` (1 day).
+	JwtTokenExpiresAfter int64 `env:"JWT_TOKEN_EXPIRES_AFTER" envDefault:"86400"`
+	// JwtTokenEncryptAlgo is a value which will be used as encrypt algo for encode the token.
+	JwtTokenEncryptAlgo string `env:"JWT_TOKEN_ENCRYPT_ALGO" envDefault:"HS256" opts:"HS256,HS384,HS512"`
 }
