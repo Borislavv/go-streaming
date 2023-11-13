37d34f93d5e84d73f74c84fb0cc86a1f4dd02f8b
Boris <Glazunov2142@icloud.com>
2023-11-13 11:47:17 +0300

    chore: expanded streaming action data models.

--- internal/infrastructure/service/streamer/action/model/data.go
+++ internal/infrastructure/service/streamer/action/model/data.go
@@ -1,11 +1,13 @@
 package model
 
 type StreamByIdData struct {
-	ID string
+	ID    string
+	Token string
 }
 
 type StreamByIdWithOffsetData struct {
 	ID       string
+	Token    string
 	From     float64
 	Duration float64
 }
