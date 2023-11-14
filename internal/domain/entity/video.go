d451efe5303807705726c1157c9b733670388e46
Boris <Glazunov2142@icloud.com>
2023-11-14 10:39:52 +0300

    chore: improved video entity.

--- internal/domain/entity/video.go
+++ internal/domain/entity/video.go
@@ -4,7 +4,7 @@ import "github.com/Borislavv/video-streaming/internal/domain/vo"
 
 type Video struct {
 	ID          vo.ID  `json:"id" bson:",inline"`
-	UserID      vo.ID  `json:"userID" bson:"userID"`
+	UserID      vo.ID  `json:"userID" bson:"user"`
 	Name        string `json:"name" bson:"name"`
 	Description string `json:"description" bson:"description,omitempty"`
 }
