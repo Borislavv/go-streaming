ccc33002471b88df1f940c685e10102d122d51b6
Boris <Glazunov2142@icloud.com>
2023-11-16 12:00:00 +0300

    chore: added password_hasher interface improved.

--- internal/domain/service/security/password_hasher.go
+++ internal/domain/service/security/password_hasher.go
@@ -1,6 +1,8 @@
 package security
 
+import "github.com/Borislavv/video-streaming/internal/domain/agg"
+
 type PasswordHasher interface {
 	Hash(password string) (hash string, err error)
-	Verify(password string) (err error)
+	Verify(userAgg *agg.User, password string) (err error)
 }
