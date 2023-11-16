861c49f54ffbd658a17a1e5d7b674c48642e690e
Boris <Glazunov2142@icloud.com>
2023-11-16 12:00:00 +0300

    chore: added password_hasher improved.

--- internal/infrastructure/service/security/password_hasher.go
+++ internal/infrastructure/service/security/password_hasher.go
@@ -1,16 +1,34 @@
 package security
 
+import (
+	"github.com/Borislavv/video-streaming/internal/domain/agg"
+	"github.com/Borislavv/video-streaming/internal/domain/logger"
+	"github.com/Borislavv/video-streaming/internal/domain/repository"
+	"golang.org/x/crypto/bcrypt"
+)
+
 type PasswordHasher struct {
+	logger     logger.Logger
+	repository repository.User
+	cost       int
 }
 
-func NewPasswordHasher() *PasswordHasher {
-	return &PasswordHasher{}
+func NewPasswordHasher(logger logger.Logger, repository repository.User, cost int) *PasswordHasher {
+	return &PasswordHasher{
+		logger:     logger,
+		repository: repository,
+		cost:       cost,
+	}
 }
 
 func (s *PasswordHasher) Hash(password string) (hash string, err error) {
-	return "", nil
+	hashBytes, err := bcrypt.GenerateFromPassword([]byte(password), s.cost)
+	if err != nil {
+		return "", s.logger.LogPropagate(err)
+	}
+	return string(hashBytes), nil
 }
 
-func (s *PasswordHasher) Verify(password string) (err error) {
-	return nil
+func (s *PasswordHasher) Verify(userAgg *agg.User, password string) (err error) {
+	return bcrypt.CompareHashAndPassword([]byte(userAgg.Password), []byte(password))
 }
