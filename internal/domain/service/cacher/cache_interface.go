d6b8917c585f59e9d063f5ba893669dff42bd8ac
Boris <Glazunov2142@icloud.com>
2023-11-20 12:00:00 +0300

    chore: added new cache interace into the domain layer.

---
+++ internal/domain/service/cacher/cache_interface.go
@@ -0,0 +1,6 @@
+package cacher
+
+type Cacher interface {
+	Get(key string, fn func(CacheItem) (data interface{}, err error)) (data interface{}, err error)
+	Delete(key string)
+}
--- internal/domain/service/cacher/cacher_interface.go
+++
@@ -1,6 +0,0 @@
-package cacher
-
-type Cacher interface {
-	Get(key string, fn func() interface{}) error
-	Delete(key string) error
-}
