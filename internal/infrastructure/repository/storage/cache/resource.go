99624760631e81a9c00ced1e39a5df621cc021e4
Boris <Glazunov2142@icloud.com>
2023-11-21 12:00:00 +0300

    chore: added sketch of cache resource repository.

--- internal/infrastructure/repository/storage/cache/resource.go
+++ internal/infrastructure/repository/storage/cache/resource.go
@@ -1,1 +1,13 @@
 package cache
+
+import (
+	"github.com/Borislavv/video-streaming/internal/domain/logger"
+	"github.com/Borislavv/video-streaming/internal/domain/service/cacher"
+	"github.com/Borislavv/video-streaming/internal/infrastructure/repository/storage/mongodb"
+)
+
+type ResourceRepository struct {
+	*mongodb.UserRepository
+	logger logger.Logger
+	cache  cacher.Cacher
+}
