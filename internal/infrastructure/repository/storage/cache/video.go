7749f1c163a29e2ab0ee29f2b41ae5f7fbfb33b8
Boris <Glazunov2142@icloud.com>
2023-11-21 12:00:00 +0300

    chore: implemented cached method which named findByID of video cache repository.

--- internal/infrastructure/repository/storage/cache/video.go
+++ internal/infrastructure/repository/storage/cache/video.go
@@ -1,9 +1,15 @@
 package cache
 
 import (
+	"context"
+	"fmt"
+	"github.com/Borislavv/video-streaming/internal/domain/agg"
+	"github.com/Borislavv/video-streaming/internal/domain/errors"
 	"github.com/Borislavv/video-streaming/internal/domain/logger"
 	"github.com/Borislavv/video-streaming/internal/domain/service/cacher"
+	"github.com/Borislavv/video-streaming/internal/infrastructure/repository/query"
 	"github.com/Borislavv/video-streaming/internal/infrastructure/repository/storage/mongodb"
+	"reflect"
 )
 
 type VideoRepository struct {
@@ -22,4 +28,33 @@ func NewVideoRepository(
 		logger:          logger,
 		cache:           cache,
 	}
+}
+
+func (r *VideoRepository) FindOneByID(ctx context.Context, q query.FindOneVideoByID) (*agg.Video, error) {
+	// building a cache key
+	cacheKey := fmt.Sprintf("videoID_%v_userID_%v ", q.GetID().Value.Hex(), q.GetUserID().Value.Hex())
+
+	// fetching data from cache/storage
+	videoInterface, err := r.cache.Get(
+		cacheKey,
+		func(item cacher.CacheItem) (data interface{}, err error) {
+			userAgg, err := r.VideoRepository.FindOneByID(ctx, q)
+			if err != nil {
+				return false, r.logger.LogPropagate(err)
+			}
+			return userAgg, nil
+		})
+	if err != nil {
+		return nil, r.logger.LogPropagate(err)
+	}
+
+	// casting found data to struct
+	videoAgg, ok := videoInterface.(*agg.Video)
+	if !ok {
+		return nil, errors.NewCachedDataTypeWasNotMatchedError(
+			cacheKey, reflect.TypeOf(&agg.User{}), reflect.TypeOf(videoInterface),
+		)
+	}
+
+	return videoAgg, nil
 }
