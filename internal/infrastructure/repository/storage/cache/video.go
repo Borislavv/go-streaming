456779bee529875eef7ed1462be144113a9eff6f
Boris <Glazunov2142@icloud.com>
2023-11-23 12:00:00 +0300

    chore: implemented new method into the video cache repository.

--- internal/infrastructure/repository/storage/cache/video.go
+++ internal/infrastructure/repository/storage/cache/video.go
@@ -2,11 +2,13 @@ package cache
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/Borislavv/video-streaming/internal/domain/agg"
 	"github.com/Borislavv/video-streaming/internal/domain/errors"
 	"github.com/Borislavv/video-streaming/internal/domain/logger"
 	"github.com/Borislavv/video-streaming/internal/domain/service/cacher"
+	"github.com/Borislavv/video-streaming/internal/infrastructure/helper"
 	"github.com/Borislavv/video-streaming/internal/infrastructure/repository/query"
 	"github.com/Borislavv/video-streaming/internal/infrastructure/repository/storage/mongodb"
 	"reflect"
@@ -43,7 +45,7 @@ func (r *VideoRepository) FindOneByID(ctx context.Context, q query.FindOneVideoB
 
 			userAgg, err := r.VideoRepository.FindOneByID(ctx, q)
 			if err != nil {
-				return false, r.logger.LogPropagate(err)
+				return nil, r.logger.LogPropagate(err)
 			}
 			return userAgg, nil
 		})
@@ -60,4 +62,46 @@ func (r *VideoRepository) FindOneByID(ctx context.Context, q query.FindOneVideoB
 	}
 
 	return videoAgg, nil
+}
+
+func (r *VideoRepository) FindList(ctx context.Context, q query.FindVideoList) (list []*agg.Video, total int64, err error) {
+	p, err := json.Marshal(q)
+	if err != nil {
+		r.logger.Log(err)
+		// return a result from mongodb
+		return r.VideoRepository.FindList(ctx, q)
+	}
+
+	cacheKey := helper.MD5(p)
+
+	type response struct {
+		List  []*agg.Video
+		Total int64
+	}
+
+	listInterface, err := r.cache.Get(
+		cacheKey,
+		func(item cacher.CacheItem) (data interface{}, err error) {
+			item.SetTTL(time.Hour)
+
+			l, t, e := r.VideoRepository.FindList(ctx, q)
+			if err != nil {
+				return nil, r.logger.LogPropagate(e)
+			}
+
+			return response{List: l, Total: t}, nil
+		},
+	)
+	if err != nil {
+		return nil, 0, r.logger.LogPropagate(err)
+	}
+
+	listResponse, ok := listInterface.(response)
+	if !ok {
+		return nil, 0, errors.NewCachedDataTypeWasNotMatchedError(
+			cacheKey, reflect.TypeOf([]*agg.Video{}), reflect.TypeOf(listInterface),
+		)
+	}
+
+	return listResponse.List, listResponse.Total, nil
 }
