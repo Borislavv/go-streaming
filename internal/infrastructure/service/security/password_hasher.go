41fd1af871506dd8e0f82460287da3797ae50f4b
Boris <Glazunov2142@icloud.com>
2023-12-05 12:00:00 +0300

    chore: improved password hasher by parameter typed as interface

--- internal/infrastructure/service/security/password_hasher.go
+++ internal/infrastructure/service/security/password_hasher.go
@@ -1,9 +1,9 @@
 package security
 
 import (
-	"github.com/Borislavv/video-streaming/internal/domain/agg"
 	"github.com/Borislavv/video-streaming/internal/domain/errors"
 	"github.com/Borislavv/video-streaming/internal/domain/logger"
+	"github.com/Borislavv/video-streaming/internal/domain/service/security"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -27,8 +27,8 @@ func (s *PasswordHasher) Hash(password string) (hash string, err error) {
 	return string(hashBytes), nil
 }
 
-func (s *PasswordHasher) Verify(userAgg *agg.User, password string) (err error) {
-	if err = bcrypt.CompareHashAndPassword([]byte(userAgg.Password), []byte(password)); err != nil {
+func (s *PasswordHasher) Verify(user security.Passwordness, password string) (err error) {
+	if err = bcrypt.CompareHashAndPassword([]byte(user.GetPassword()), []byte(password)); err != nil {
 		if err == bcrypt.ErrMismatchedHashAndPassword {
 			return errors.NewAuthFailedError("passwords did not match")
 		} else {
