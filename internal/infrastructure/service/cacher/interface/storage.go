7b1a7b7ab498facf82d48b3f3b09756428dadcc9
Boris <Glazunov2142@icloud.com>
2023-12-08 12:00:00 +0300

    chore: cacher/interface/storage refactored.

--- internal/infrastructure/service/cacher/interface/storage.go
+++ internal/infrastructure/service/cacher/interface/storage.go
@@ -1,4 +1,6 @@
-package cacher
+package cacher_interface
+
+import cacher_interface "github.com/Borislavv/video-streaming/internal/domain/service/cacher/interface"
 
 type Storage interface {
 	Get(key string, fn func(cacher_interface.CacheItem) (data interface{}, err error)) (data interface{}, err error)
