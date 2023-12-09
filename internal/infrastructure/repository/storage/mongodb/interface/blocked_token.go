f0b47e9eacd7da0dc00af60c61951927daab8a17
Boris <Glazunov2142@icloud.com>
2023-12-09 12:00:00 +0300

    chore: mongodb/interface/blocked_token refactored.

--- internal/infrastructure/repository/storage/mongodb/interface/blocked_token.go
+++ internal/infrastructure/repository/storage/mongodb/interface/blocked_token.go
@@ -1,1 +1,11 @@
 package mongodb_interface
+
+import (
+	"context"
+	"github.com/Borislavv/video-streaming/internal/domain/agg"
+)
+
+type BlockedToken interface {
+	Insert(ctx context.Context, token *agg.BlockedToken) error
+	Has(ctx context.Context, token string) (found bool, err error)
+}
