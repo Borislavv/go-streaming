4fcba240ce89ef6655e78d91d3bd28c5386be8c0
Boris <Glazunov2142@icloud.com>
2023-12-09 12:00:00 +0300

    chore: authenticator/interface/authenticator refactored.

--- internal/domain/service/authenticator/interface/authenticator.go
+++ internal/domain/service/authenticator/interface/authenticator.go
@@ -1,14 +1,14 @@
-package authenticator
+package authenticator_interface
 
 import (
-	"github.com/Borislavv/video-streaming/internal/domain/dto"
+	dto_interface "github.com/Borislavv/video-streaming/internal/domain/dto/interface"
 	"github.com/Borislavv/video-streaming/internal/domain/vo"
 	"net/http"
 )
 
 type Authenticator interface {
 	// Auth will check raw credentials and generate a new access token for given user.
-	Auth(reqDTO dto.AuthRequest) (token string, err error)
+	Auth(reqDTO dto_interface.AuthRequest) (token string, err error)
 	// IsAuthed with check that token is valid and extract userID from it.
 	IsAuthed(r *http.Request) (userID vo.ID, err error)
 }
