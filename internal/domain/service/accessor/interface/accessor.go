20f7450de0ea9a7b2d9d7203e31e9223a8ade112
Boris <Glazunov2142@icloud.com>
2023-12-09 12:00:00 +0300

    chore: accessor/interface/accessor refactored.

--- internal/domain/service/accessor/interface/accessor.go
+++ internal/domain/service/accessor/interface/accessor.go
@@ -1,11 +1,11 @@
-package _interface
+package accessor_interface
 
 import (
-	"github.com/Borislavv/video-streaming/internal/domain/agg"
+	agg_interface "github.com/Borislavv/video-streaming/internal/domain/agg/interface"
 	"github.com/Borislavv/video-streaming/internal/domain/vo"
 )
 
 type Accessor interface {
 	// IsGranted is a method which will check the access to target aggregates scope.
-	IsGranted(userID vo.ID, aggregates ...agg.Aggregate) error
+	IsGranted(userID vo.ID, aggregates ...agg_interface.Aggregate) error
 }
