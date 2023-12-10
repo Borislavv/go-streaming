7c36a178fc1f1bd2617d66f3eb3c5042efa3ff67
Boris <Glazunov2142@icloud.com>
2023-12-10 12:00:00 +0300

    chore: user/interface/crud refactored.

--- internal/domain/service/user/interface/crud.go
+++ internal/domain/service/user/interface/crud.go
@@ -1,13 +1,13 @@
-package _interface
+package user_interface
 
 import (
 	"github.com/Borislavv/video-streaming/internal/domain/agg"
-	"github.com/Borislavv/video-streaming/internal/domain/dto"
+	"github.com/Borislavv/video-streaming/internal/domain/dto/interface"
 )
 
 type CRUD interface {
-	Get(reqDTO dto.GetUserRequest) (*agg.User, error)
-	Create(reqDTO dto.CreateUserRequest) (*agg.User, error)
-	Update(reqDTO dto.UpdateUserRequest) (*agg.User, error)
-	Delete(reqDTO dto.DeleteUserRequest) error
+	Get(reqDTO dto_interface.GetUserRequest) (*agg.User, error)
+	Create(reqDTO dto_interface.CreateUserRequest) (*agg.User, error)
+	Update(reqDTO dto_interface.UpdateUserRequest) (*agg.User, error)
+	Delete(reqDTO dto_interface.DeleteUserRequest) error
 }
