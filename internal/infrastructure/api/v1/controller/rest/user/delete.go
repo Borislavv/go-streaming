557145ff3b12113b7f91bde3d2b4c77bd2fc21fa
Boris <Glazunov2142@icloud.com>
2023-12-11 12:00:00 +0300

    chore: controller/rest/user/delete refactored.

--- internal/infrastructure/api/v1/controller/rest/user/delete.go
+++ internal/infrastructure/api/v1/controller/rest/user/delete.go
@@ -1,53 +1,69 @@
 package user
 
 import (
-	"github.com/Borislavv/video-streaming/internal/domain/builder"
-	"github.com/Borislavv/video-streaming/internal/domain/logger"
-	"github.com/Borislavv/video-streaming/internal/domain/service/user"
-	"github.com/Borislavv/video-streaming/internal/infrastructure/api/v1/response"
+	builder_interface "github.com/Borislavv/video-streaming/internal/domain/builder/interface"
+	"github.com/Borislavv/video-streaming/internal/domain/logger/interface"
+	"github.com/Borislavv/video-streaming/internal/domain/service/di/interface"
+	user_interface "github.com/Borislavv/video-streaming/internal/domain/service/user/interface"
+	response_interface "github.com/Borislavv/video-streaming/internal/infrastructure/api/v1/response/interface"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
 const DeletePath = "/user/{id}"
 
-type DeleteUserController struct {
-	logger   logger.Logger
-	builder  builder.User
-	service  user.CRUD
-	response response.Responder
+type DeleteController struct {
+	logger    logger_interface.Logger
+	builder   builder_interface.User
+	service   user_interface.CRUD
+	responder response_interface.Responder
 }
 
-func NewDeleteController(
-	logger logger.Logger,
-	builder builder.User,
-	service user.CRUD,
-	response response.Responder,
-) *DeleteUserController {
-	return &DeleteUserController{
-		logger:   logger,
-		builder:  builder,
-		service:  service,
-		response: response,
+func NewDeleteController(serviceContainer di_interface.ContainerManager) (*DeleteController, error) {
+	loggerService, err := serviceContainer.GetLoggerService()
+	if err != nil {
+		return nil, err
 	}
+
+	userBuilder, err := serviceContainer.GetUserBuilder()
+	if err != nil {
+		return nil, loggerService.LogPropagate(err)
+	}
+
+	userCRUDService, err := serviceContainer.GetUserCRUDService()
+	if err != nil {
+		return nil, loggerService.LogPropagate(err)
+	}
+
+	responseService, err := serviceContainer.GetResponderService()
+	if err != nil {
+		return nil, loggerService.LogPropagate(err)
+	}
+
+	return &DeleteController{
+		logger:    loggerService,
+		builder:   userBuilder,
+		service:   userCRUDService,
+		responder: responseService,
+	}, nil
 }
 
-func (c *DeleteUserController) Delete(w http.ResponseWriter, r *http.Request) {
+func (c *DeleteController) Delete(w http.ResponseWriter, r *http.Request) {
 	reqDTO, err := c.builder.BuildDeleteRequestDTOFromRequest(r)
 	if err != nil {
-		c.response.Respond(w, c.logger.LogPropagate(err))
+		c.responder.Respond(w, c.logger.LogPropagate(err))
 		return
 	}
 
 	if err = c.service.Delete(reqDTO); err != nil {
-		c.response.Respond(w, c.logger.LogPropagate(err))
+		c.responder.Respond(w, c.logger.LogPropagate(err))
 		return
 	}
 
 	w.WriteHeader(http.StatusNoContent)
 }
 
-func (c *DeleteUserController) AddRoute(router *mux.Router) {
+func (c *DeleteController) AddRoute(router *mux.Router) {
 	router.
 		Path(DeletePath).
 		HandlerFunc(c.Delete).
