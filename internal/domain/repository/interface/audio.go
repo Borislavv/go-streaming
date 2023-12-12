fb06ead31020fe0fb181b088dca13ef166e251f3
Boris <Glazunov2142@icloud.com>
2023-12-12 13:31:09 +0300

    chore: repository/interface/audio refactored.

--- internal/domain/repository/interface/audio.go
+++ internal/domain/repository/interface/audio.go
@@ -1,9 +1,9 @@
-package _interface
+package repository_interface
 
 import (
 	"context"
 	"github.com/Borislavv/video-streaming/internal/domain/agg"
-	"github.com/Borislavv/video-streaming/internal/domain/dto"
+	dto_interface "github.com/Borislavv/video-streaming/internal/domain/dto/interface"
 	"github.com/Borislavv/video-streaming/internal/domain/vo"
 )
 
@@ -11,5 +11,5 @@ type Audio interface {
 	Insert(ctx context.Context, audio *agg.Audio) (string, error)
 	Update(ctx context.Context, audio *agg.Audio) error
 	Find(ctx context.Context, id vo.ID) (*agg.Audio, error)
-	FindList(ctx context.Context, dto dto.ListAudioRequest) ([]*agg.Audio, error)
+	FindList(ctx context.Context, dto dto_interface.ListAudioRequest) ([]*agg.Audio, error)
 }
