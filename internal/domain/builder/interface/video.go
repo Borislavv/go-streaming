3023d04e6a509724b163ca94f35245e87f3695ee
Boris <Glazunov2142@icloud.com>
2023-12-12 13:34:07 +0300

    chore: refactored interface/video.

--- internal/domain/builder/interface/video.go
+++ internal/domain/builder/interface/video.go
@@ -1,8 +1,9 @@
-package _interface
+package builder_interface
 
 import (
 	"github.com/Borislavv/video-streaming/internal/domain/agg"
 	"github.com/Borislavv/video-streaming/internal/domain/dto"
+	dto_interface "github.com/Borislavv/video-streaming/internal/domain/dto/interface"
 	"net/http"
 )
 
@@ -10,8 +11,8 @@ type Video interface {
 	BuildGetRequestDTOFromRequest(r *http.Request) (*dto.VideoGetRequestDTO, error)
 	BuildListRequestDTOFromRequest(r *http.Request) (*dto.VideoListRequestDTO, error)
 	BuildCreateRequestDTOFromRequest(r *http.Request) (*dto.VideoCreateRequestDTO, error)
-	BuildAggFromCreateRequestDTO(reqDTO dto.CreateVideoRequest) (*agg.Video, error)
+	BuildAggFromCreateRequestDTO(reqDTO dto_interface.CreateVideoRequest) (*agg.Video, error)
 	BuildUpdateRequestDTOFromRequest(r *http.Request) (*dto.VideoUpdateRequestDTO, error)
-	BuildAggFromUpdateRequestDTO(reqDTO dto.UpdateVideoRequest) (*agg.Video, error)
+	BuildAggFromUpdateRequestDTO(reqDTO dto_interface.UpdateVideoRequest) (*agg.Video, error)
 	BuildDeleteRequestDTOFromRequest(r *http.Request) (*dto.VideoDeleteRequestDto, error)
 }
