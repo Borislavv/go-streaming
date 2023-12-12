ae9f9a08e9a48f7d96130221f557f30687a7979d
Boris <Glazunov2142@icloud.com>
2023-12-12 13:34:32 +0300

    chore: refactored builder/interface/resource.

--- internal/domain/builder/interface/resource.go
+++ internal/domain/builder/interface/resource.go
@@ -1,12 +1,13 @@
-package _interface
+package builder_interface
 
 import (
 	"github.com/Borislavv/video-streaming/internal/domain/agg"
 	"github.com/Borislavv/video-streaming/internal/domain/dto"
+	"github.com/Borislavv/video-streaming/internal/domain/dto/interface"
 	"net/http"
 )
 
 type Resource interface {
 	BuildUploadRequestDTOFromRequest(r *http.Request) (*dto.ResourceUploadRequestDTO, error)
-	BuildAggFromUploadRequestDTO(reqDTO dto.UploadResourceRequest) *agg.Resource
+	BuildAggFromUploadRequestDTO(reqDTO dto_interface.UploadResourceRequest) *agg.Resource
 }
