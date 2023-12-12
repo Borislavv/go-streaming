655d8b6d1661c7bf140f88633a5104e101e9a796
Boris <Glazunov2142@icloud.com>
2023-12-12 13:30:13 +0300

    chore: repository/interface/video refactored.

--- internal/domain/repository/interface/video.go
+++ internal/domain/repository/interface/video.go
@@ -1,16 +1,16 @@
-package _interface
+package repository_interface
 
 import (
 	"context"
 	"github.com/Borislavv/video-streaming/internal/domain/agg"
-	"github.com/Borislavv/video-streaming/internal/infrastructure/repository/query"
+	"github.com/Borislavv/video-streaming/internal/infrastructure/repository/query/interface"
 )
 
 type Video interface {
-	FindOneByID(ctx context.Context, q query.FindOneVideoByID) (*agg.Video, error)
-	FindOneByName(ctx context.Context, q query.FindOneVideoByName) (*agg.Video, error)
-	FindOneByResourceID(ctx context.Context, q query.FindOneVideoByResourceID) (*agg.Video, error)
-	FindList(ctx context.Context, q query.FindVideoList) (list []*agg.Video, total int64, err error)
+	FindOneByID(ctx context.Context, q query_interface.FindOneVideoByID) (*agg.Video, error)
+	FindOneByName(ctx context.Context, q query_interface.FindOneVideoByName) (*agg.Video, error)
+	FindOneByResourceID(ctx context.Context, q query_interface.FindOneVideoByResourceID) (*agg.Video, error)
+	FindList(ctx context.Context, q query_interface.FindVideoList) (list []*agg.Video, total int64, err error)
 	Insert(ctx context.Context, video *agg.Video) (*agg.Video, error)
 	Update(ctx context.Context, video *agg.Video) (*agg.Video, error)
 	Remove(ctx context.Context, video *agg.Video) error
