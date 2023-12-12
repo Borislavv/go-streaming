41e1bb3de01b13de9d99f9b49d2274a785a2181c
Boris <Glazunov2142@icloud.com>
2023-12-12 13:33:52 +0300

    chore: refactored builder/resource.

--- internal/domain/builder/resource.go
+++ internal/domain/builder/resource.go
@@ -3,30 +3,38 @@ package builder
 import (
 	"github.com/Borislavv/video-streaming/internal/domain/agg"
 	"github.com/Borislavv/video-streaming/internal/domain/dto"
+	"github.com/Borislavv/video-streaming/internal/domain/dto/interface"
 	"github.com/Borislavv/video-streaming/internal/domain/entity"
 	"github.com/Borislavv/video-streaming/internal/domain/enum"
-	"github.com/Borislavv/video-streaming/internal/domain/logger"
+	"github.com/Borislavv/video-streaming/internal/domain/logger/interface"
+	"github.com/Borislavv/video-streaming/internal/domain/service/di/interface"
 	"github.com/Borislavv/video-streaming/internal/domain/vo"
 	"net/http"
 	"time"
 )
 
 type ResourceBuilder struct {
-	logger                    logger.Logger
+	logger                    logger_interface.Logger
 	formFilename              string
 	inMemoryFileSizeThreshold int64
 }
 
-func NewResourceBuilder(
-	logger logger.Logger,
-	formFilename string,
-	inMemoryFileSizeThreshold int64,
-) *ResourceBuilder {
-	return &ResourceBuilder{
-		logger:                    logger,
-		formFilename:              formFilename,
-		inMemoryFileSizeThreshold: inMemoryFileSizeThreshold,
+func NewResourceBuilder(serviceContainer di_interface.ContainerManager) (*ResourceBuilder, error) {
+	loggerService, err := serviceContainer.GetLoggerService()
+	if err != nil {
+		return nil, err
 	}
+
+	cfg, err := serviceContainer.GetConfig()
+	if err != nil {
+		return nil, loggerService.LogPropagate(err)
+	}
+
+	return &ResourceBuilder{
+		logger:                    loggerService,
+		formFilename:              cfg.ResourceFormFilename,
+		inMemoryFileSizeThreshold: cfg.ResourceInMemoryFileSizeThreshold,
+	}, nil
 }
 
 // BuildUploadRequestDTOFromRequest will be parse raw *http.Request and build a dto.UploadResourceRequest
@@ -39,7 +47,7 @@ func (b *ResourceBuilder) BuildUploadRequestDTOFromRequest(r *http.Request) (*dt
 }
 
 // BuildAggFromUploadRequestDTO will be make an agg.Resource from dto.UploadResourceRequest
-func (b *ResourceBuilder) BuildAggFromUploadRequestDTO(req dto.UploadResourceRequest) *agg.Resource {
+func (b *ResourceBuilder) BuildAggFromUploadRequestDTO(req dto_interface.UploadResourceRequest) *agg.Resource {
 	return &agg.Resource{
 		Resource: entity.Resource{
 			UserID:   req.GetUserID(),
