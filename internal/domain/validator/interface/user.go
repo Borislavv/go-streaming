692f0af0a121c00a73faf0ee7a29f73e24b44751
Boris <Glazunov2142@icloud.com>
2024-01-28 11:23:47 +0300

    chore: refactoring of packages naming.

--- internal/domain/validator/interface/user.go
+++ internal/domain/validator/interface/user.go
@@ -1,14 +1,14 @@
-package validator_interface
+package validatorinterface
 
 import (
 	"github.com/Borislavv/video-streaming/internal/domain/agg"
-	dto_interface "github.com/Borislavv/video-streaming/internal/domain/dto/interface"
+	dtointerface "github.com/Borislavv/video-streaming/internal/domain/dto/interface"
 )
 
 type User interface {
-	ValidateGetRequestDTO(reqDTO dto_interface.GetUserRequest) error
-	ValidateCreateRequestDTO(reqDTO dto_interface.CreateUserRequest) error
-	ValidateUpdateRequestDTO(reqDTO dto_interface.UpdateUserRequest) error
-	ValidateDeleteRequestDTO(reqDTO dto_interface.DeleteUserRequest) error
+	ValidateGetRequestDTO(reqDTO dtointerface.GetUserRequest) error
+	ValidateCreateRequestDTO(reqDTO dtointerface.CreateUserRequest) error
+	ValidateUpdateRequestDTO(reqDTO dtointerface.UpdateUserRequest) error
+	ValidateDeleteRequestDTO(reqDTO dtointerface.DeleteUserRequest) error
 	ValidateAggregate(agg *agg.User) error
 }
