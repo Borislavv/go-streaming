6bbb51e70f60a060f5debced3d9332556488f484
Boris <Glazunov2142@icloud.com>
2024-01-28 11:23:47 +0300

    chore: refactoring of packages naming.

--- internal/domain/service/video/interface/crud.go
+++ internal/domain/service/video/interface/crud.go
@@ -1,14 +1,14 @@
-package video_interface
+package videointerface
 
 import (
 	"github.com/Borislavv/video-streaming/internal/domain/agg"
-	"github.com/Borislavv/video-streaming/internal/domain/dto/interface"
+	dtointerface "github.com/Borislavv/video-streaming/internal/domain/dto/interface"
 )
 
 type CRUD interface {
-	Get(reqDTO dto_interface.GetVideoRequest) (*agg.Video, error)
-	List(reqDTO dto_interface.ListVideoRequest) (list []*agg.Video, total int64, err error)
-	Create(reqDTO dto_interface.CreateVideoRequest) (*agg.Video, error)
-	Update(reqDTO dto_interface.UpdateVideoRequest) (*agg.Video, error)
-	Delete(reqDTO dto_interface.DeleteVideoRequest) error
+	Get(reqDTO dtointerface.GetVideoRequest) (*agg.Video, error)
+	List(reqDTO dtointerface.ListVideoRequest) (list []*agg.Video, total int64, err error)
+	Create(reqDTO dtointerface.CreateVideoRequest) (*agg.Video, error)
+	Update(reqDTO dtointerface.UpdateVideoRequest) (*agg.Video, error)
+	Delete(reqDTO dtointerface.DeleteVideoRequest) error
 }
