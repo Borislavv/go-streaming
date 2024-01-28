f43bacf9ad5ec39b12b2ce03432c832289bf563a
Boris <Glazunov2142@icloud.com>
2024-01-28 11:23:47 +0300

    chore: refactoring of packages naming.

--- internal/domain/validator/interface/resource.go
+++ internal/domain/validator/interface/resource.go
@@ -1,14 +1,14 @@
-package validator_interface
+package validatorinterface
 
 import (
 	"github.com/Borislavv/video-streaming/internal/domain/agg"
-	dto_interface "github.com/Borislavv/video-streaming/internal/domain/dto/interface"
+	dtointerface "github.com/Borislavv/video-streaming/internal/domain/dto/interface"
 	"github.com/Borislavv/video-streaming/internal/domain/entity"
 )
 
 type Resource interface {
-	ValidateUploadRequestDTO(req dto_interface.UploadResourceRequest) error
+	ValidateUploadRequestDTO(req dtointerface.UploadResourceRequest) error
 	ValidateEntity(entity entity.Resource) error
 	ValidateAggregate(agg *agg.Resource) error
-	ValidateDeleteRequestDTO(req dto_interface.DeleteResourceRequest) error
+	ValidateDeleteRequestDTO(req dtointerface.DeleteResourceRequest) error
 }
