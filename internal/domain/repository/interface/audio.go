db185803fa001fe4328d62d2bfe547e0edb485d5
Boris <Glazunov2142@icloud.com>
2024-02-23 11:23:47 +0300

    chore: refactoring of packages naming.

--- internal/domain/repository/interface/audio.go
+++ internal/domain/repository/interface/audio.go
@@ -3,7 +3,7 @@ package repositoryinterface
 import (
 	"context"
 	"github.com/Borislavv/video-streaming/internal/domain/agg"
-	dto_interface "github.com/Borislavv/video-streaming/internal/domain/dto/interface"
+	dtointerface "github.com/Borislavv/video-streaming/internal/domain/dto/interface"
 	"github.com/Borislavv/video-streaming/internal/domain/vo"
 )
 
@@ -11,5 +11,5 @@ type Audio interface {
 	Insert(ctx context.Context, audio *agg.Audio) (string, error)
 	Update(ctx context.Context, audio *agg.Audio) error
 	Find(ctx context.Context, id vo.ID) (*agg.Audio, error)
-	FindList(ctx context.Context, dto dto_interface.ListAudioRequest) ([]*agg.Audio, error)
+	FindList(ctx context.Context, dto dtointerface.ListAudioRequest) ([]*agg.Audio, error)
 }
