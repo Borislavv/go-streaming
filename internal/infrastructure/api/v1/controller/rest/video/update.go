3105c6341b49f47c92daf392c04847129797f04b
Boris <Glazunov2142@icloud.com>
2024-02-24 11:23:47 +0300

    chore: refactoring of packages naming.

--- internal/infrastructure/api/v1/controller/rest/video/update.go
+++ internal/infrastructure/api/v1/controller/rest/video/update.go
@@ -1,11 +1,11 @@
 package video
 
 import (
-	"github.com/Borislavv/video-streaming/internal/domain/builder/interface"
+	builderinterface "github.com/Borislavv/video-streaming/internal/domain/builder/interface"
 	"github.com/Borislavv/video-streaming/internal/domain/logger/interface"
 	"github.com/Borislavv/video-streaming/internal/domain/service/di/interface"
-	video_interface "github.com/Borislavv/video-streaming/internal/domain/service/video/interface"
-	response_interface "github.com/Borislavv/video-streaming/internal/infrastructure/api/v1/response/interface"
+	videointerface "github.com/Borislavv/video-streaming/internal/domain/service/video/interface"
+	responseinterface "github.com/Borislavv/video-streaming/internal/infrastructure/api/v1/response/interface"
 	"github.com/gorilla/mux"
 	"net/http"
 )
@@ -14,9 +14,9 @@ const UpdatePath = "/video/{id}"
 
 type UpdateController struct {
 	logger   loggerinterface.Logger
-	builder  builder_interface.Video
-	service  video_interface.CRUD
-	response response_interface.Responder
+	builder  builderinterface.Video
+	service  videointerface.CRUD
+	response responseinterface.Responder
 }
 
 func NewUpdateController(serviceContainer diinterface.ContainerManager) (*UpdateController, error) {
