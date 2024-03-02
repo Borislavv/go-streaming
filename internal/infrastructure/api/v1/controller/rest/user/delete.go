21ef800b01850df4b175f43cb55eb6a72a1fa480
Boris <Glazunov2142@icloud.com>
2024-03-02 09:34:47 +0300

    chore: refactoring of packages naming.

--- internal/infrastructure/api/v1/controller/rest/user/delete.go
+++ internal/infrastructure/api/v1/controller/rest/user/delete.go
@@ -13,7 +13,7 @@ import (
 const DeletePath = "/user/{id}"
 
 type DeleteController struct {
-	logger    logger_interface.Logger
+	logger    loggerinterface.Logger
 	builder   builder_interface.User
 	service   user_interface.CRUD
 	responder response_interface.Responder
