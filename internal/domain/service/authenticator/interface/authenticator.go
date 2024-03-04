eef4a3e237d1f21488be50d9b9b03aa6403989a6
Boris <Glazunov2142@icloud.com>
2024-03-04 11:23:47 +0300

    chore: refactoring of packages naming.

--- internal/domain/service/authenticator/interface/authenticator.go
+++ internal/domain/service/authenticator/interface/authenticator.go
@@ -1,14 +1,14 @@
-package authenticator_interface
+package authenticatorinterface
 
 import (
-	dto_interface "github.com/Borislavv/video-streaming/internal/domain/dto/interface"
+	dtointerface "github.com/Borislavv/video-streaming/internal/domain/dto/interface"
 	"github.com/Borislavv/video-streaming/internal/domain/vo"
 	"net/http"
 )
 
 type Authenticator interface {
 	// Auth will check raw credentials and generate a new access token for given user.
-	Auth(reqDTO dto_interface.AuthRequest) (token string, err error)
+	Auth(reqDTO dtointerface.AuthRequest) (token string, err error)
 	// IsAuthed with check that token is valid and extract userID from it.
 	IsAuthed(r *http.Request) (userID vo.ID, err error)
 }
