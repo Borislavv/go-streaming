dcdba966222967df2d526abedfa8b13e49d2f749
Boris <Glazunov2142@icloud.com>
2024-03-05 09:34:47 +0300

    chore: refactoring of packages naming.

--- internal/infrastructure/repository/storage/mongodb/interface/resource.go
+++ internal/infrastructure/repository/storage/mongodb/interface/resource.go
@@ -1,13 +1,13 @@
-package mongodb_interface
+package mongodbinterface
 
 import (
 	"context"
 	"github.com/Borislavv/video-streaming/internal/domain/agg"
-	query_interface "github.com/Borislavv/video-streaming/internal/infrastructure/repository/query/interface"
+	queryinterface "github.com/Borislavv/video-streaming/internal/infrastructure/repository/query/interface"
 )
 
 type Resource interface {
-	FindOneByID(context.Context, query_interface.FindOneResourceByID) (*agg.Resource, error)
+	FindOneByID(context.Context, queryinterface.FindOneResourceByID) (*agg.Resource, error)
 	Insert(context.Context, *agg.Resource) (*agg.Resource, error)
 	Remove(context.Context, *agg.Resource) error
 }
