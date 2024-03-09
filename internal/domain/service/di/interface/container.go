28104de690c643c0cdd8071a635d7a6ffe59ac2a
Boris <Glazunov2142@icloud.com>
2024-03-09 09:34:47 +0300

    chore: refactoring of packages naming.

--- internal/domain/service/di/interface/container.go
+++ internal/domain/service/di/interface/container.go
@@ -3,32 +3,32 @@ package diinterface
 import (
 	"context"
 	"github.com/Borislavv/video-streaming/internal/app"
-	builder_interface "github.com/Borislavv/video-streaming/internal/domain/builder/interface"
+	builderinterface "github.com/Borislavv/video-streaming/internal/domain/builder/interface"
 	"github.com/Borislavv/video-streaming/internal/domain/logger/interface"
-	repository_interface "github.com/Borislavv/video-streaming/internal/domain/repository/interface"
-	accessor_interface "github.com/Borislavv/video-streaming/internal/domain/service/accessor/interface"
-	authenticator_interface "github.com/Borislavv/video-streaming/internal/domain/service/authenticator/interface"
-	cacher_interface "github.com/Borislavv/video-streaming/internal/domain/service/cacher/interface"
-	extractor_interface "github.com/Borislavv/video-streaming/internal/domain/service/extractor/interface"
+	repositoryinterface "github.com/Borislavv/video-streaming/internal/domain/repository/interface"
+	accessorinterface "github.com/Borislavv/video-streaming/internal/domain/service/accessor/interface"
+	authenticatorinterface "github.com/Borislavv/video-streaming/internal/domain/service/authenticator/interface"
+	cacherinterface "github.com/Borislavv/video-streaming/internal/domain/service/cacher/interface"
+	extractorinterface "github.com/Borislavv/video-streaming/internal/domain/service/extractor/interface"
 	resourceservice "github.com/Borislavv/video-streaming/internal/domain/service/resource/interface"
-	security_interface "github.com/Borislavv/video-streaming/internal/domain/service/security/interface"
-	tokenizer_interface "github.com/Borislavv/video-streaming/internal/domain/service/tokenizer/interface"
-	uploader_interface "github.com/Borislavv/video-streaming/internal/domain/service/uploader/interface"
+	securityinterface "github.com/Borislavv/video-streaming/internal/domain/service/security/interface"
+	tokenizerinterface "github.com/Borislavv/video-streaming/internal/domain/service/tokenizer/interface"
+	uploaderinterface "github.com/Borislavv/video-streaming/internal/domain/service/uploader/interface"
 	userservice "github.com/Borislavv/video-streaming/internal/domain/service/user/interface"
 	videoservice "github.com/Borislavv/video-streaming/internal/domain/service/video/interface"
-	validator_interface "github.com/Borislavv/video-streaming/internal/domain/validator/interface"
-	response_interface "github.com/Borislavv/video-streaming/internal/infrastructure/api/v1/response/interface"
+	validatorinterface "github.com/Borislavv/video-streaming/internal/domain/validator/interface"
+	responseinterface "github.com/Borislavv/video-streaming/internal/infrastructure/api/v1/response/interface"
 	diinterface "github.com/Borislavv/video-streaming/internal/infrastructure/di/interface"
-	cache_interface "github.com/Borislavv/video-streaming/internal/infrastructure/repository/storage/cache/interface"
+	cacheinterface "github.com/Borislavv/video-streaming/internal/infrastructure/repository/storage/cache/interface"
 	mongodbinterface "github.com/Borislavv/video-streaming/internal/infrastructure/repository/storage/mongodb/interface"
-	detector_interface "github.com/Borislavv/video-streaming/internal/infrastructure/service/detector/interface"
-	reader_interface "github.com/Borislavv/video-streaming/internal/infrastructure/service/reader/interface"
-	handler_interface "github.com/Borislavv/video-streaming/internal/infrastructure/service/streamer/action/handler/interface"
-	strategy_interface "github.com/Borislavv/video-streaming/internal/infrastructure/service/streamer/action/handler/strategy/interface"
-	listener_interface "github.com/Borislavv/video-streaming/internal/infrastructure/service/streamer/action/listener/interface"
-	streamer_interface "github.com/Borislavv/video-streaming/internal/infrastructure/service/streamer/interface"
-	proto_interface "github.com/Borislavv/video-streaming/internal/infrastructure/service/streamer/proto/interface"
-	file_interface "github.com/Borislavv/video-streaming/internal/infrastructure/service/uploader/file/interface"
+	detectorinterface "github.com/Borislavv/video-streaming/internal/infrastructure/service/detector/interface"
+	readerinterface "github.com/Borislavv/video-streaming/internal/infrastructure/service/reader/interface"
+	handlerinterface "github.com/Borislavv/video-streaming/internal/infrastructure/service/streamer/action/handler/interface"
+	strategyinterface "github.com/Borislavv/video-streaming/internal/infrastructure/service/streamer/action/handler/strategy/interface"
+	listenerinterface "github.com/Borislavv/video-streaming/internal/infrastructure/service/streamer/action/listener/interface"
+	streamerinterface "github.com/Borislavv/video-streaming/internal/infrastructure/service/streamer/interface"
+	protointerface "github.com/Borislavv/video-streaming/internal/infrastructure/service/streamer/proto/interface"
+	fileinterface "github.com/Borislavv/video-streaming/internal/infrastructure/service/uploader/file/interface"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -50,62 +50,62 @@ type ContainerManager interface {
 	GetBlockedTokenMongoRepository() (mongodbinterface.BlockedToken, error)
 
 	// Cache repository
-	GetResourceCacheRepository() (cache_interface.Resource, error)
-	GetVideoCacheRepository() (cache_interface.Video, error)
-	GetUserCacheRepository() (cache_interface.User, error)
+	GetResourceCacheRepository() (cacheinterface.Resource, error)
+	GetVideoCacheRepository() (cacheinterface.Video, error)
+	GetUserCacheRepository() (cacheinterface.User, error)
 
 	// Common services
-	GetAccessService() (accessor_interface.Accessor, error)
+	GetAccessService() (accessorinterface.Accessor, error)
 
 	// Resource services
-	GetResourceBuilder() (builder_interface.Resource, error)
-	GetResourceValidator() (validator_interface.Resource, error)
-	GetResourceRepository() (repository_interface.Resource, error)
+	GetResourceBuilder() (builderinterface.Resource, error)
+	GetResourceValidator() (validatorinterface.Resource, error)
+	GetResourceRepository() (repositoryinterface.Resource, error)
 	GetResourceCRUDService() (resourceservice.CRUD, error)
 
 	// BlockedToken services
-	GetBlockedTokenRepository() (repository_interface.BlockedToken, error)
+	GetBlockedTokenRepository() (repositoryinterface.BlockedToken, error)
 
 	// Video services
-	GetVideoBuilder() (builder_interface.Video, error)
-	GetVideoValidator() (validator_interface.Video, error)
-	GetVideoRepository() (repository_interface.Video, error)
+	GetVideoBuilder() (builderinterface.Video, error)
+	GetVideoValidator() (validatorinterface.Video, error)
+	GetVideoRepository() (repositoryinterface.Video, error)
 	GetVideoCRUDService() (videoservice.CRUD, error)
 
 	// User services
-	GetUserBuilder() (builder_interface.User, error)
-	GetUserValidator() (validator_interface.User, error)
-	GetUserRepository() (repository_interface.User, error)
+	GetUserBuilder() (builderinterface.User, error)
+	GetUserValidator() (validatorinterface.User, error)
+	GetUserRepository() (repositoryinterface.User, error)
 	GetUserCRUDService() (userservice.CRUD, error)
 
 	// Auth services
-	GetAuthBuilder() (builder_interface.Auth, error)
-	GetAuthValidator() (validator_interface.Auth, error)
-	GetAuthService() (authenticator_interface.Authenticator, error)
+	GetAuthBuilder() (builderinterface.Auth, error)
+	GetAuthValidator() (validatorinterface.Auth, error)
+	GetAuthService() (authenticatorinterface.Authenticator, error)
 
 	// Infrastructure
-	GetLoggerService() (logger_interface.Logger, error)
-	GetCacheService() (cacher_interface.Cacher, error)
-	GetRequestParametersExtractorService() (extractor_interface.RequestParams, error)
-	GetResponderService() (response_interface.Responder, error)
-	GetPasswordHasherService() (security_interface.PasswordHasher, error)
-	GetTokenizerService() (tokenizer_interface.Tokenizer, error)
+	GetLoggerService() (loggerinterface.Logger, error)
+	GetCacheService() (cacherinterface.Cacher, error)
+	GetRequestParametersExtractorService() (extractorinterface.RequestParams, error)
+	GetResponderService() (responseinterface.Responder, error)
+	GetPasswordHasherService() (securityinterface.PasswordHasher, error)
+	GetTokenizerService() (tokenizerinterface.Tokenizer, error)
 
 	// File
-	GetFileStorageService() (file_interface.Storage, error)
-	GetFileNameComputerService() (file_interface.NameComputer, error)
-	GetFileUploaderService() (uploader_interface.Uploader, error)
-	GetFileReaderService() (reader_interface.FileReader, error)
+	GetFileStorageService() (fileinterface.Storage, error)
+	GetFileNameComputerService() (fileinterface.NameComputer, error)
+	GetFileUploaderService() (uploaderinterface.Uploader, error)
+	GetFileReaderService() (readerinterface.FileReader, error)
 
 	// WebSocket
-	GetWebSocketCommunicatorService() (proto_interface.Communicator, error)
-	GetWebSocketListener() (listener_interface.ActionsListener, error)
-	GetWebSocketHandler() (handler_interface.ActionsHandler, error)
-	GetWebSocketHandlerStrategies() ([]strategy_interface.ActionStrategy, error)
+	GetWebSocketCommunicatorService() (protointerface.Communicator, error)
+	GetWebSocketListener() (listenerinterface.ActionsListener, error)
+	GetWebSocketHandler() (handlerinterface.ActionsHandler, error)
+	GetWebSocketHandlerStrategies() ([]strategyinterface.ActionStrategy, error)
 
 	// Codesc
-	GetCodecsDetectorService() (detector_interface.Codecs, error)
+	GetCodecsDetectorService() (detectorinterface.Codecs, error)
 
 	// Streaming
-	GetStreamingService() (streamer_interface.Streamer, error)
+	GetStreamingService() (streamerinterface.Streamer, error)
 }
