682d038a85f3c3c384e7acc3d04c52e9bd88af9e
Boris <Glazunov2142@icloud.com>
2024-03-17 11:23:47 +0300

    chore: refactoring of packages naming.

--- internal/infrastructure/service/cacher/cache.go
+++ internal/infrastructure/service/cacher/cache.go
@@ -1,16 +1,16 @@
 package cacher
 
 import (
-	domain_cacher_interface "github.com/Borislavv/video-streaming/internal/domain/service/cacher/interface"
-	cacher_interface "github.com/Borislavv/video-streaming/internal/infrastructure/service/cacher/interface"
+	domain_cacherinterface "github.com/Borislavv/video-streaming/internal/domain/service/cacher/interface"
+	cacherinterface "github.com/Borislavv/video-streaming/internal/infrastructure/service/cacher/interface"
 )
 
 type Cache struct {
-	storage   cacher_interface.Storage
-	displacer cacher_interface.Displacer
+	storage   cacherinterface.Storage
+	displacer cacherinterface.Displacer
 }
 
-func NewCache(storage cacher_interface.Storage, displacer cacher_interface.Displacer) *Cache {
+func NewCache(storage cacherinterface.Storage, displacer cacherinterface.Displacer) *Cache {
 	c := &Cache{
 		storage:   storage,
 		displacer: displacer,
@@ -19,7 +19,7 @@ func NewCache(storage cacher_interface.Storage, displacer cacher_interface.Displ
 	return c
 }
 
-func (c *Cache) Get(key string, fn func(item domain_cacher_interface.CacheItem) (data interface{}, err error)) (data interface{}, err error) {
+func (c *Cache) Get(key string, fn func(item domain_cacherinterface.CacheItem) (data interface{}, err error)) (data interface{}, err error) {
 	return c.storage.Get(key, fn)
 }
 
