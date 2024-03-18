428c53b23c01f5c0babe7db8f8301bb2b04183bd
Boris <Glazunov2142@icloud.com>
2024-03-18 09:34:47 +0300

    chore: refactoring of packages naming.

--- internal/infrastructure/repository/storage/cache/interface/user.go
+++ internal/infrastructure/repository/storage/cache/interface/user.go
@@ -3,12 +3,12 @@ package cache_interface
 import (
 	"context"
 	"github.com/Borislavv/video-streaming/internal/domain/agg"
-	query_interface "github.com/Borislavv/video-streaming/internal/infrastructure/repository/query/interface"
+	queryinterface "github.com/Borislavv/video-streaming/internal/infrastructure/repository/query/interface"
 )
 
 type User interface {
-	FindOneByID(context.Context, query_interface.FindOneUserByID) (*agg.User, error)
-	FindOneByEmail(context.Context, query_interface.FindOneUserByEmail) (*agg.User, error)
+	FindOneByID(context.Context, queryinterface.FindOneUserByID) (*agg.User, error)
+	FindOneByEmail(context.Context, queryinterface.FindOneUserByEmail) (*agg.User, error)
 	Insert(context.Context, *agg.User) (*agg.User, error)
 	Update(context.Context, *agg.User) (*agg.User, error)
 	Remove(context.Context, *agg.User) error
