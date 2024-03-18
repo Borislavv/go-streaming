da5234b7dd461ff04dd392ca135cf17dbdcd8124
Boris <Glazunov2142@icloud.com>
2024-03-18 09:34:47 +0300

    chore: refactoring of packages naming.

--- internal/infrastructure/repository/storage/cache/interface/resource.go
+++ internal/infrastructure/repository/storage/cache/interface/resource.go
@@ -3,11 +3,11 @@ package cache_interface
 import (
 	"context"
 	"github.com/Borislavv/video-streaming/internal/domain/agg"
-	query_interface "github.com/Borislavv/video-streaming/internal/infrastructure/repository/query/interface"
+	queryinterface "github.com/Borislavv/video-streaming/internal/infrastructure/repository/query/interface"
 )
 
 type Resource interface {
-	FindOneByID(context.Context, query_interface.FindOneResourceByID) (*agg.Resource, error)
+	FindOneByID(context.Context, queryinterface.FindOneResourceByID) (*agg.Resource, error)
 	Insert(context.Context, *agg.Resource) (*agg.Resource, error)
 	Remove(context.Context, *agg.Resource) error
 }
