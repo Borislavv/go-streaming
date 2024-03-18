701eb781d5d2f779a0f08637d0fb76d4ac93ad80
Boris <Glazunov2142@icloud.com>
2024-03-18 09:34:47 +0300

    chore: refactoring of packages naming.

--- internal/infrastructure/repository/storage/cache/user.go
+++ internal/infrastructure/repository/storage/cache/user.go
@@ -7,20 +7,20 @@ import (
 	"github.com/Borislavv/video-streaming/internal/domain/errors"
 	"github.com/Borislavv/video-streaming/internal/domain/logger/interface"
 	"github.com/Borislavv/video-streaming/internal/domain/service/cacher/interface"
-	di_interface "github.com/Borislavv/video-streaming/internal/domain/service/di/interface"
+	diinterface "github.com/Borislavv/video-streaming/internal/domain/service/di/interface"
 	"github.com/Borislavv/video-streaming/internal/infrastructure/helper"
-	mongodb_interface "github.com/Borislavv/video-streaming/internal/infrastructure/repository/storage/mongodb/interface"
+	mongodbinterface "github.com/Borislavv/video-streaming/internal/infrastructure/repository/storage/mongodb/interface"
 	"reflect"
 	"time"
 )
 
 type UserRepository struct {
-	mongodb_interface.User
+	mongodbinterface.User
 	logger logger_interface.Logger
 	cache  cacher_interface.Cacher
 }
 
-func NewUserRepository(serviceContainer di_interface.ContainerManager) (*UserRepository, error) {
+func NewUserRepository(serviceContainer diinterface.ContainerManager) (*UserRepository, error) {
 	loggerService, err := serviceContainer.GetLoggerService()
 	if err != nil {
 		return nil, err
