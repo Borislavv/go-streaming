572b77837171e76ab1fcc8d1a3fa3f09f72a5855
Boris <Glazunov2142@icloud.com>
2024-03-20 11:23:47 +0300

    chore: refactoring of packages naming.

--- internal/infrastructure/service/streamer/proto/interface/communicator.go
+++ internal/infrastructure/service/streamer/proto/interface/communicator.go
@@ -1,14 +1,14 @@
-package proto_interface
+package protointerface
 
 import (
-	dto_interface "github.com/Borislavv/video-streaming/internal/domain/dto/interface"
+	dtointerface "github.com/Borislavv/video-streaming/internal/domain/dto/interface"
 	"github.com/Borislavv/video-streaming/internal/infrastructure/service/streamer/action/enum"
 	"github.com/gorilla/websocket"
 )
 
 type Communicator interface {
 	Start(audioCodec string, videoCodec string, conn *websocket.Conn) error
-	Send(chunk dto_interface.Chunk, conn *websocket.Conn) error
+	Send(chunk dtointerface.Chunk, conn *websocket.Conn) error
 	Parse(bytes []byte) (action enum.Actions, data interface{}, err error)
 	Error(err error, conn *websocket.Conn) error
 	Stop(conn *websocket.Conn) error
