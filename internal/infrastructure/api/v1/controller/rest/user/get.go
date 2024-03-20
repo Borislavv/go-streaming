98fb98a318fc6d1a671e29ee5c1e6a9fa089b7de
Boris <Glazunov2142@icloud.com>
2024-03-20 09:34:47 +0300

    chore: refactoring of packages naming.

--- internal/infrastructure/api/v1/controller/rest/user/get.go
+++ internal/infrastructure/api/v1/controller/rest/user/get.go
@@ -31,7 +31,7 @@ type GetController struct {
 	responder response_interface.Responder
 }
 
-func NewGetController(serviceContainer di_interface.ContainerManager) (*GetController, error) {
+func NewGetController(serviceContainer diinterface.ContainerManager) (*GetController, error) {
 	loggerService, err := serviceContainer.GetLoggerService()
 	if err != nil {
 		return nil, err
