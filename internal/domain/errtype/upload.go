58b421dc24d4b9b265d3684b3c29f46d47322530
Boris <glazunov2142@icloud.com>
2024-03-30 15:27:49 +0300

    chore: errors->errtype package refactoring.

---
+++ internal/domain/errtype/upload.go
@@ -0,0 +1,28 @@
+package errtype
+
+import (
+	"fmt"
+	"github.com/Borislavv/video-streaming/internal/infrastructure/service/logger"
+	"net/http"
+)
+
+const (
+	uploadErrType         = "application"
+	publicUploadErrLevel  = logger.WarningLevel
+	publicUploadErrStatus = http.StatusBadRequest
+)
+
+type ResourceAlreadyExistsError struct{ publicError }
+
+func NewResourceAlreadyExistsError(name string) *ResourceAlreadyExistsError {
+	return &ResourceAlreadyExistsError{
+		publicError{
+			errored{
+				ErrorMessage: fmt.Sprintf("the resource '%v' being loaded already exists", name),
+				ErrorType:    uploadErrType,
+				errorStatus:  publicUploadErrStatus,
+				errorLevel:   publicUploadErrLevel,
+			},
+		},
+	}
+}
