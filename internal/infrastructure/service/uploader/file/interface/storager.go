fbcb300f4c12ca8c5ae4ac9e58d7143c992388fa
Boris <glazunov2142@icloud.com>
2024-04-09 11:44:42 +0300

    chore: improved storage interface.

--- internal/infrastructure/service/uploader/file/interface/storager.go
+++ internal/infrastructure/service/uploader/file/interface/storager.go
@@ -9,7 +9,7 @@ type Storage interface {
 	// Has is checking whether the file already exists.
 	Has(userID vo.ID, filename string) (has bool, err error)
 	// Store is saving file and calculating new hashed name.
-	Store(userID vo.ID, name string, reader io.Reader) (length int64, filename string, filepath string, err error)
+	Store(userID vo.ID, name string, reader io.Reader) (length int64, filepath string, err error)
 	// Remove is delete the file by name from resources directory.
 	Remove(userID vo.ID, name string) (err error)
 }
