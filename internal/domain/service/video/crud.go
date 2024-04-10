2a230db8283119ad6eaa2c1474068b9ba1931b27
Boris <glazunov2142@icloud.com>
2024-04-10 08:40:54 +0300

    chore: video crud.

--- internal/domain/service/video/crud.go
+++ internal/domain/service/video/crud.go
@@ -22,7 +22,7 @@ type CRUDService struct {
 	resourceService resourceinterface.CRUD
 }
 
-func NewCRUDService(serviceContainer diinterface.ContainerManager) (*CRUDService, error) {
+func NewCRUDService(serviceContainer diinterface.ServiceContainer) (*CRUDService, error) {
 	loggerService, err := serviceContainer.GetLoggerService()
 	if err != nil {
 		return nil, err
